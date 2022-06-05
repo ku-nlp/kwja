7e60821824ba8ea1e7c21699edc13f0ff1590661
Taka008 <kodama@nlp.ist.i.kyoto-u.ac.jp>
2022-06-05 11:00:09 +0900

    add labels to CharDataset

--- src/jula/datamodule/datasets/char_dataset.py
+++ src/jula/datamodule/datasets/char_dataset.py
@@ -3,6 +3,7 @@ from rhoknp import Document
 from transformers import BatchEncoding
 
 from jula.datamodule.datasets.base_dataset import BaseDataset
+from jula.utils.utils import SEG_LABEL2INDEX
 
 
 class CharDataset(BaseDataset):
@@ -18,7 +19,24 @@ class CharDataset(BaseDataset):
         )
         input_ids = encoding["input_ids"]
         attention_mask = encoding["attention_mask"]
+
+        seg_labels: list[int] = []
+        for morpheme in document.morphemes:
+            seg_labels.extend(
+                [SEG_LABEL2INDEX["B"]]
+                + [SEG_LABEL2INDEX["I"]] * (len(morpheme.text) - 1)
+            )
+        seg_labels = (
+            [SEG_LABEL2INDEX["PAD"]]
+            + seg_labels[: self.max_seq_length - 2]
+            + [SEG_LABEL2INDEX["PAD"]]
+        )
+        seg_labels = seg_labels + [SEG_LABEL2INDEX["PAD"]] * (
+            self.max_seq_length - len(seg_labels)
+        )
+
         return {
             "input_ids": torch.tensor(input_ids, dtype=torch.long),
             "attention_mask": torch.tensor(attention_mask, dtype=torch.long),
+            "seg_labels": torch.tensor(seg_labels, dtype=torch.long),
         }
