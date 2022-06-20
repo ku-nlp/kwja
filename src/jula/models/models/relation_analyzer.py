8ff96b2d3c057042825960fe88acea582b12cf7b
omura <omura@nlp.ist.i.kyoto-u.ac.jp>
2022-06-20 20:12:35 +0900

    add dependency parser

---
+++ src/jula/models/models/relation_analyzer.py
@@ -0,0 +1,60 @@
+from typing import Optional
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from omegaconf import DictConfig
+from transformers import PretrainedConfig
+
+from jula.models.models.dependency_parser import DependencyParser
+from jula.utils.utils import IGNORE_INDEX
+
+
+class RelationAnalyzer(nn.Module):
+    def __init__(
+        self, hparams: DictConfig, pretrained_model_config: PretrainedConfig
+    ) -> None:
+        super().__init__()
+        self.hparams = hparams
+
+        self.dependency_parser: DependencyParser = DependencyParser(
+            pretrained_model_config=pretrained_model_config
+        )
+
+    def forward(
+        self,
+        pooled_outputs: torch.Tensor,
+        batch: dict[str, torch.Tensor],
+        inference: Optional[bool] = False,
+    ) -> dict[str, torch.Tensor]:
+        output: dict[str, torch.Tensor] = dict()
+        # (batch_size, max_seq_len, max_seq_len)
+        dependency_logits, dependency_type_logits = self.dependency_parser(
+            pooled_outputs, dependencies=None if inference else batch["dependencies"]
+        )
+        dependency_logits = torch.where(
+            batch["intra_mask"],
+            dependency_logits,
+            torch.full_like(dependency_logits, -1e4),
+        )
+        output.update(
+            {
+                "dependency_logits": dependency_logits,
+                "dependency_type_logits": dependency_type_logits,
+            }
+        )
+        if "dependencies" in batch:
+            dependency_loss = F.cross_entropy(
+                input=dependency_logits.view(-1, dependency_logits.size(2)),
+                target=batch["dependencies"].view(-1),
+                ignore_index=IGNORE_INDEX,
+            )
+            output.update({"dependency_loss": dependency_loss})
+        if "dependency_types" in batch:
+            dependency_type_loss = F.cross_entropy(
+                input=dependency_type_logits.view(-1, dependency_type_logits.size(2)),
+                target=batch["dependency_types"].view(-1),
+                ignore_index=IGNORE_INDEX,
+            )
+            output.update({"dependency_type_loss": dependency_type_loss})
+        return output
--- src/jula/models/word_module.py
+++ src/jula/models/word_module.py
@@ -6,9 +6,11 @@ from omegaconf import DictConfig
 from pytorch_lightning.core.lightning import LightningModule
 from transformers import AutoTokenizer, PreTrainedTokenizerBase
 
+from jula.evaluators.dependency_parsing_metric import DependencyParsingMetric
 from jula.evaluators.phrase_analysis_metric import PhraseAnalysisMetric
 from jula.models.models.phrase_analyzer import PhraseAnalyzer
 from jula.models.models.pooling import PoolingStrategy
+from jula.models.models.relation_analyzer import RelationAnalyzer
 from jula.models.models.word_encoder import WordEncoder
 
 
@@ -41,22 +43,57 @@ class WordModule(LightningModule):
             corpus: PhraseAnalysisMetric() for corpus in self.test_corpora
         }
 
-    def forward(self, **batch) -> dict[str, dict[str, torch.Tensor]]:
+        self.relation_analyzer: RelationAnalyzer = RelationAnalyzer(
+            hparams=hparams,
+            pretrained_model_config=self.word_encoder.pretrained_model.config,
+        )
+        self.valid_dependency_parsing_metrics: dict[str, DependencyParsingMetric] = {
+            corpus: DependencyParsingMetric() for corpus in self.valid_corpora
+        }
+        self.test_dependency_parsing_metrics: dict[str, DependencyParsingMetric] = {
+            corpus: DependencyParsingMetric() for corpus in self.test_corpora
+        }
+
+    def forward(self, inference=False, **batch) -> dict[str, dict[str, torch.Tensor]]:
         # (batch_size, seq_len, hidden_size)
         pooled_outputs = self.word_encoder(batch, PoolingStrategy.FIRST)
         phrase_analyzer_outputs = self.phrase_analyzer(pooled_outputs, batch)
-        return {"phrase_analyzer_outputs": phrase_analyzer_outputs}
+        relation_analyzer_output = self.relation_analyzer(
+            pooled_outputs, batch, inference=inference
+        )
+        return {
+            "phrase_analyzer_outputs": phrase_analyzer_outputs,
+            "relation_analyzer_outputs": relation_analyzer_output,
+        }
 
     def training_step(self, batch: Any, batch_idx: int) -> torch.Tensor:
-        outputs: dict[str, torch.Tensor] = self(**batch)
-        loss = outputs["phrase_analyzer_outputs"]["phrase_analysis_loss"]
-        self.log("train/phrase_analysis_loss", loss, on_step=True, on_epoch=True)
-        return loss
+        outputs: dict[str, torch.Tensor] = self(inference=False, **batch)
+        phrase_analysis_loss = outputs["phrase_analyzer_outputs"][
+            "phrase_analysis_loss"
+        ]
+        self.log(
+            "train/phrase_analysis_loss",
+            phrase_analysis_loss,
+            on_step=True,
+            on_epoch=True,
+        )
+        dependency_loss = outputs["relation_analyzer_outputs"]["dependency_loss"]
+        self.log("train/dependency_loss", dependency_loss, on_step=True, on_epoch=True)
+        dependency_type_loss = outputs["relation_analyzer_outputs"][
+            "dependency_type_loss"
+        ]
+        self.log(
+            "train/dependency_type_loss",
+            dependency_type_loss,
+            on_step=True,
+            on_epoch=True,
+        )
+        return phrase_analysis_loss + dependency_loss + dependency_type_loss
 
     def validation_step(
         self, batch: Any, batch_idx: int, dataloader_idx: Optional[int] = None
     ) -> None:
-        outputs: dict[str, torch.Tensor] = self(**batch)
+        outputs: dict[str, torch.Tensor] = self(inference=True, **batch)
         corpus = self.valid_corpora[dataloader_idx or 0]
         phrase_analysis_metric_args = {
             "document_ids": batch["document_id"],
@@ -73,6 +110,23 @@ class WordModule(LightningModule):
             outputs["phrase_analyzer_outputs"]["phrase_analysis_loss"],
         )
 
+        dependency_parsing_metric_args = {
+            "document_ids": batch["document_id"],
+            "preds": torch.argmax(
+                outputs["relation_analyzer_outputs"]["dependency_logits"], dim=2
+            ),
+            "type_preds": torch.argmax(
+                outputs["relation_analyzer_outputs"]["dependency_type_logits"], dim=2
+            ),
+        }
+        self.valid_dependency_parsing_metrics[corpus].update(
+            **dependency_parsing_metric_args
+        )
+        self.log(
+            "valid/dependency_loss",
+            outputs["relation_analyzer_outputs"]["dependency_loss"],
+        )
+
     def validation_epoch_end(self, validation_step_outputs) -> None:
         f1 = 0.0
         for corpus, metric in self.valid_phrase_analysis_metrics.items():
@@ -83,10 +137,16 @@ class WordModule(LightningModule):
             metric.reset()
         self.log("valid/f1", f1 / len(self.valid_phrase_analysis_metrics))
 
+        for corpus, metric in self.valid_dependency_parsing_metrics.items():
+            dataset = self.trainer.datamodule.valid_datasets[corpus]
+            for name, value in metric.compute(dataset).items():
+                self.log(f"valid_{corpus}/{name}", value)
+            metric.reset()
+
     def test_step(
         self, batch: Any, batch_idx: int, dataloader_idx: Optional[int] = None
     ) -> None:
-        outputs: dict[str, torch.Tensor] = self(**batch)
+        outputs: dict[str, torch.Tensor] = self(inference=True, **batch)
         corpus = self.test_corpora[dataloader_idx or 0]
         phrase_analysis_metric_args = {
             "document_ids": batch["document_id"],
@@ -103,6 +163,23 @@ class WordModule(LightningModule):
             outputs["phrase_analyzer_outputs"]["phrase_analysis_loss"],
         )
 
+        dependency_parsing_metric_args = {
+            "document_ids": batch["document_id"],
+            "preds": torch.argmax(
+                outputs["relation_analyzer_outputs"]["dependency_logits"], dim=2
+            ),
+            "type_preds": torch.argmax(
+                outputs["relation_analyzer_outputs"]["dependency_type_logits"], dim=2
+            ),
+        }
+        self.test_dependency_parsing_metrics[corpus].update(
+            **dependency_parsing_metric_args
+        )
+        self.log(
+            "test/dependency_loss",
+            outputs["relation_analyzer_outputs"]["dependency_loss"],
+        )
+
     def test_epoch_end(self, test_step_outputs) -> None:
         f1 = 0.0
         for corpus, metric in self.test_phrase_analysis_metrics.items():
@@ -113,16 +190,28 @@ class WordModule(LightningModule):
             metric.reset()
         self.log("test/f1", f1 / len(self.valid_phrase_analysis_metrics))
 
+        for corpus, metric in self.test_dependency_parsing_metrics.items():
+            dataset = self.trainer.datamodule.test_datasets[corpus]
+            for name, value in metric.compute(dataset).items():
+                self.log(f"test_{corpus}/{name}", value)
+            metric.reset()
+
     def predict_step(
         self, batch: Any, batch_idx: int, dataloader_idx: Optional[int] = None
     ) -> Any:
-        outputs: dict[str, torch.Tensor] = self(**batch)
+        outputs: dict[str, torch.Tensor] = self(inference=True, **batch)
         return {
             "document_ids": batch["document_id"],
             "phrase_analysis_logits": outputs["phrase_analyzer_outputs"][
                 "phrase_analysis_logits"
             ],
             "base_phrase_features": batch["base_phrase_features"],
+            "dependency_logits": outputs["relation_analyzer_outputs"][
+                "dependency_logits"
+            ],
+            "dependency_type_logits": outputs["relation_analyzer_outputs"][
+                "dependency_type_logits"
+            ],
         }
 
     def configure_optimizers(self):
