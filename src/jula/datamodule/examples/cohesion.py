972343fd3aa76eec7cef6671fbe559c6c76dfd2a
nobu-g <ueda-n127@hotmail.co.jp>
2022-06-21 17:17:01 +0900

    add cohesion example

---
+++ src/jula/datamodule/examples/cohesion.py
@@ -0,0 +1,108 @@
+import logging
+from enum import Enum
+from typing import Optional
+
+from kyoto_reader import Document
+from tokenizers import Encoding
+
+from jula.datamodule.extractors import (
+    Annotation,
+    BridgingExtractor,
+    CoreferenceExtractor,
+    PasExtractor,
+)
+from jula.datamodule.extractors.base import Extractor, Mrph, Phrase
+
+logger = logging.getLogger(__file__)
+
+
+class Task(Enum):
+    PAS_ANALYSIS = "pas_analysis"
+    BRIDGING = "bridging"
+    COREFERENCE = "coreference"
+
+
+TASK2EXTRACTOR = {
+    Task.PAS_ANALYSIS: PasExtractor,
+    Task.BRIDGING: BridgingExtractor,
+    Task.COREFERENCE: CoreferenceExtractor,
+}
+
+
+class CohesionExample:
+    """A single training/test example for bridging anaphora resolution."""
+
+    def __init__(self) -> None:
+        self.example_id: int = -1
+        self.doc_id: str = ""
+        self.annotations: dict[Task, Annotation] = {}
+        self.phrases: dict[Task, list[Phrase]] = {}
+        self.encoding: Optional[Encoding] = None
+
+    @property
+    def mrphs(self) -> dict[Task, list[Mrph]]:
+        return {
+            task: sum((p.children for p in phrases), [])
+            for task, phrases in self.phrases.items()
+        }
+
+    def load(
+        self,
+        document: Document,
+        tasks: list[Task],
+        extractors: dict[Task, Extractor],
+    ):
+        self.doc_id = document.doc_id
+        for task in tasks:
+            phrases = self._construct_phrases(document)
+            extractor: Extractor = extractors[task]
+            self.phrases[task] = phrases
+            self.annotations[task] = extractor.extract(
+                document, phrases
+            )  # extract gold
+
+    @staticmethod
+    def _construct_phrases(document: Document) -> list[Phrase]:
+        # construct phrases and mrphs
+        phrases = []
+        for sentence in document.sentences:
+            for anaphor in sentence.bps:
+                phrase = Phrase(
+                    dtid=anaphor.dtid,
+                    surf=anaphor.surf,
+                    dmids=anaphor.dmids,
+                    dmid=anaphor.dmid,
+                    children=[],
+                    is_target=False,
+                    candidates=[],
+                )
+                for morpheme in anaphor.mrph_list():
+                    mrph = Mrph(
+                        dmid=document.mrph2dmid[morpheme],
+                        surf=morpheme.midasi,
+                        parent=phrase,
+                    )
+                    phrase.children.append(mrph)
+                phrases.append(phrase)
+        return phrases
+
+    def __str__(self):
+        return self.__repr__()
+
+    def __repr__(self):
+        string = ""
+        for i, phrase in enumerate(next(iter(self.phrases.values()))):
+            string += f"{i:02} {phrase.surf}"
+            string += " " * (6 - len(phrase.surf)) * 2
+            if ann := self.annotations.get(Task.PAS_ANALYSIS):
+                for case, args in ann.arguments_set[i].items():
+                    args_str = ",".join(str(arg) for arg in args)
+                    string += f"{case}:{args_str:6}  "
+            if ann := self.annotations.get(Task.BRIDGING):
+                args_str = ",".join(str(arg) for arg in ann.arguments_set[i])
+                string += f"ノ:{args_str:6}  "
+            if ann := self.annotations.get(Task.COREFERENCE):
+                args_str = ",".join(str(arg) for arg in ann.arguments_set[i])
+                string += f"＝:{args_str:6}"
+            string += "\n"
+        return string
