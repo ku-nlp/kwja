464d94e9cbb3c18143f3d24bff5daf04f9e37b5e
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2022-06-21 21:04:58 +0900

    refactor WordSegmenterMetric

--- src/jula/evaluators/word_segment.py
+++ src/jula/evaluators/word_segment.py
@@ -12,6 +12,26 @@ class WordSegmenterMetric:
         pass
 
     @staticmethod
+    def convert_ids_to_labels(ids: list[int]) -> list[str]:
+        return [
+            INDEX2SEG_LABEL[id_] if id_ != SEG_LABEL2INDEX["PAD"] else "O"
+            for id_ in ids
+        ]
+
+    @staticmethod
+    def filter_predictions(
+        preds: list[str], labels: list[str]
+    ) -> Tuple[list[str], list[str]]:
+        filtered_preds = []
+        filtered_labels = []
+        for pred, label in zip(preds, labels):
+            if label == "O":
+                continue
+            filtered_preds.append(pred)
+            filtered_labels.append(label)
+        return filtered_preds, filtered_labels
+
+    @staticmethod
     def convert_num2label(
         preds: list[list[int]], labels: list[list[int]]
     ) -> Tuple[list[list[str]], list[list[str]]]:
@@ -41,14 +61,28 @@ class WordSegmenterMetric:
             if "_loss" in key:
                 metrics[key] = outputs[key].detach()
 
-        seg_preds, seg_labels = self.convert_num2label(
-            preds=torch.argmax(outputs["logits"], dim=-1).cpu().tolist(),
-            labels=batch["seg_labels"].cpu().tolist(),
-        )  # (b, seq_len), (b, seq_len)
-        metrics["acc"] = accuracy_score(y_true=seg_labels, y_pred=seg_preds)
+        seg_preds = [
+            self.convert_ids_to_labels(ids)
+            for ids in torch.argmax(outputs["logits"], dim=-1).cpu().tolist()
+        ]
+        seg_labels = [
+            self.convert_ids_to_labels(ids)
+            for ids in batch["seg_labels"].cpu().tolist()
+        ]
+        filtered_seg_preds = []
+        filtered_seg_labels = []
+        for seg_preds_item, seg_labels_item in zip(seg_preds, seg_labels):
+            filtered_seg_preds_item, filtered_seg_labels_item = self.filter_predictions(
+                seg_preds_item, seg_labels_item
+            )
+            filtered_seg_preds.append(filtered_seg_preds_item)
+            filtered_seg_labels.append(filtered_seg_labels_item)
+        metrics["acc"] = accuracy_score(
+            y_true=filtered_seg_labels, y_pred=filtered_seg_preds
+        )
         metrics["f1"] = f1_score(
-            y_true=seg_labels,
-            y_pred=seg_preds,
+            y_true=filtered_seg_labels,
+            y_pred=filtered_seg_preds,
             mode="strict",
             scheme=IOB2,
             zero_division=0,
