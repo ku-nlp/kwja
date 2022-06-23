7115897512f7d3979dda557c21a6b9815bb27cb0
Taka008 <kodama@nlp.ist.i.kyoto-u.ac.jp>
2022-06-23 12:38:32 +0900

    add positional embedding to word segmenter

--- src/jula/datamodule/datasets/char_dataset.py
+++ src/jula/datamodule/datasets/char_dataset.py
@@ -1,14 +1,62 @@
+import pickle
+
+import dartsclone
 import torch
 from rhoknp import Document
 from transformers import BatchEncoding
 
 from jula.datamodule.datasets.base_dataset import BaseDataset
-from jula.utils.utils import IGNORE_INDEX, SEG_TYPES
+from jula.utils.utils import ENE_TYPE_BIES, IGNORE_INDEX, SEG_TYPES
 
 
 class CharDataset(BaseDataset):
+    def __init__(
+        self,
+        wiki_ene_dic_path: str,
+        max_ene_num: int = 0,
+        **kwargs,
+    ) -> None:
+        super().__init__(**kwargs)
+
+        self.max_ene_num = max_ene_num
+        self.darts = dartsclone.DoubleArray()
+        self.darts.open(f"{wiki_ene_dic_path}/wiki.da")
+        self.values: list[list[str]] = pickle.load(
+            open(f"{wiki_ene_dic_path}/wiki_values.pkl", "rb")
+        )
+
     def __getitem__(self, index: int) -> dict[str, torch.Tensor]:
         return self.encode(self.documents[index])
+
+    def get_ene_ids(self, text: str) -> list[list[int]]:
+        pos2ene_ids: dict[int, list[int]] = {
+            pos: [] for pos in range(self.max_seq_length)
+        }
+        for char_pos in range(0, len(text) - 1):
+            subtext: bytes = text[char_pos:].encode("utf-8")
+            for match_idx, match_len in self.darts.common_prefix_search(subtext):
+                match_word_len: int = len(subtext[0:match_len].decode("utf-8"))
+                for i in range(match_word_len):
+                    pos_in_match_word: int = char_pos + i
+                    for ene_type in self.values[match_idx]:
+                        if i == 0:
+                            bie = "B"
+                        elif i == match_word_len - 1:
+                            bie = "E"
+                        else:
+                            bie = "I"
+                        ene_id: int = ENE_TYPE_BIES.index(f"{bie}-{ene_type}")
+                        if ene_id not in pos2ene_ids[pos_in_match_word]:
+                            pos2ene_ids[pos_in_match_word].append(ene_id)
+        ene_ids: list[list[int]] = [
+            [ENE_TYPE_BIES.index("PAD")] * self.max_seq_length
+            for _ in range(self.max_ene_num)
+        ]  # (max_ene_num, max_seq_length)
+        for pos in pos2ene_ids.keys():
+            for idx, ene_id in enumerate(pos2ene_ids[pos][: self.max_ene_num]):
+                ene_ids[idx][pos] = ene_id
+
+        return ene_ids
 
     def encode(self, document: Document) -> dict[str, torch.Tensor]:
         encoding: BatchEncoding = self.tokenizer(
@@ -33,8 +81,18 @@ class CharDataset(BaseDataset):
             self.max_seq_length - len(seg_labels)
         )
 
+        raw_ene_ids: list[list[int]] = self.get_ene_ids(document.text)
+        ene_ids: list[list[int]] = []
+        for raw_ene_id in raw_ene_ids:
+            ene_ids.append(
+                [ENE_TYPE_BIES.index("PAD")]
+                + raw_ene_id[: self.max_seq_length - 2]
+                + [ENE_TYPE_BIES.index("PAD")]
+            )
+
         return {
             "input_ids": torch.tensor(input_ids, dtype=torch.long),
             "attention_mask": torch.tensor(attention_mask, dtype=torch.long),
+            "ene_ids": torch.tensor(ene_ids, dtype=torch.long),
             "seg_labels": torch.tensor(seg_labels, dtype=torch.long),
         }
--- src/jula/models/models/char_encoder.py
+++ src/jula/models/models/char_encoder.py
@@ -3,6 +3,8 @@ import torch.nn as nn
 from omegaconf import DictConfig
 from transformers import AutoModel, PreTrainedTokenizer
 
+from jula.utils.utils import ENE_TYPE_BIES
+
 
 class CharEncoder(nn.Module):
     def __init__(self, hparams: DictConfig, tokenizer: PreTrainedTokenizer) -> None:
@@ -14,10 +16,32 @@ class CharEncoder(nn.Module):
         )
         self.pretrained_model.resize_token_embeddings(len(tokenizer))
 
+        self.max_ene_num: int = self.hparams.dataset.max_ene_num
+        if self.max_ene_num > 0:
+            self.ene_pos_embed: nn.Embedding = nn.Embedding(
+                len(ENE_TYPE_BIES), self.pretrained_model.config.hidden_size
+            )
+
     def forward(self, inputs: dict[str, torch.Tensor]) -> dict[str, torch.Tensor]:
-        outputs = self.pretrained_model(
-            input_ids=inputs["input_ids"],
-            attention_mask=inputs["attention_mask"],
-        )
+        if self.max_ene_num > 0:
+            batch_size, ene_num, seq_len = inputs["ene_ids"].size()
+            ene_embed = self.ene_pos_embed(
+                inputs["ene_ids"].view(-1, seq_len)
+            )  # (b * ene_num, seq_len, h)
+            ene_embed_sum = torch.sum(
+                ene_embed.reshape(batch_size, ene_num, seq_len, -1), dim=1
+            )  # (b, seq_len, h)
+            inputs_embeds = self.pretrained_model.embeddings.word_embeddings(
+                inputs["input_ids"]
+            )  # (b, seq_len, h)
+            outputs = self.pretrained_model(
+                attention_mask=inputs["attention_mask"],
+                inputs_embeds=inputs_embeds + ene_embed_sum,
+            )
+        else:
+            outputs = self.pretrained_model(
+                input_ids=inputs["input_ids"],
+                attention_mask=inputs["attention_mask"],
+            )
 
         return outputs.last_hidden_state  # (b, seq_len, h)
