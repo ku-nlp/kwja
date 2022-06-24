395c1e4aa05544305d7b275e2a8df678211719e2
Taka008 <kodama@nlp.ist.i.kyoto-u.ac.jp>
2022-06-24 17:50:08 +0900

    add add_ene_embed_before_encoder flag

--- src/jula/models/models/char_encoder.py
+++ src/jula/models/models/char_encoder.py
@@ -22,34 +22,48 @@ class CharEncoder(nn.Module):
 
         self.max_ene_num: int = self.hparams.dataset.max_ene_num
         if self.max_ene_num > 0:
-            self.ene_pos_embed: nn.Embedding = nn.Embedding(
-                len(ENE_TYPE_BIES), self.pretrained_model.config.hidden_size
+            self.add_ene_embed_before_encoder: bool = (
+                hparams.add_ene_embed_before_encoder
+            )
+            self.ene_embed: nn.Embedding = nn.Embedding(
+                len(ENE_TYPE_BIES),
+                self.pretrained_model.config.hidden_size,
+                padding_idx=ENE_TYPE_BIES.index("PAD"),
             )
 
     def forward(self, inputs: dict[str, torch.Tensor]) -> dict[str, torch.Tensor]:
         if self.max_ene_num > 0:
             batch_size, ene_num, seq_len = inputs["ene_ids"].size()
-            ene_embed = self.ene_pos_embed(
+            ene_embed = self.ene_embed(
                 inputs["ene_ids"].view(-1, seq_len)
             )  # (b * ene_num, seq_len, h)
             ene_embed_sum = torch.sum(
                 ene_embed.reshape(batch_size, ene_num, seq_len, -1), dim=1
             )  # (b, seq_len, h)
 
-            position_ids = create_position_ids_from_input_ids(
-                inputs["input_ids"],
-                padding_idx=self.pretrained_model.config.pad_token_id,
-            )
-            inputs_embeds = self.word_embed(inputs["input_ids"])  # (b, seq_len, h)
-            outputs = self.pretrained_model(
-                attention_mask=inputs["attention_mask"],
-                position_ids=position_ids,
-                inputs_embeds=inputs_embeds + ene_embed_sum,
-            )
+            if self.add_ene_embed_before_encoder:
+                position_ids = create_position_ids_from_input_ids(
+                    inputs["input_ids"],
+                    padding_idx=self.pretrained_model.config.pad_token_id,
+                )
+                inputs_embeds = self.word_embed(inputs["input_ids"])  # (b, seq_len, h)
+                outputs = self.pretrained_model(
+                    attention_mask=inputs["attention_mask"],
+                    position_ids=position_ids,
+                    inputs_embeds=inputs_embeds + ene_embed_sum,
+                )
+                encoder_output = outputs.last_hidden_state
+            else:
+                outputs = self.pretrained_model(
+                    input_ids=inputs["input_ids"],
+                    attention_mask=inputs["attention_mask"],
+                )
+                encoder_output = outputs.last_hidden_state + ene_embed_sum
         else:
             outputs = self.pretrained_model(
                 input_ids=inputs["input_ids"],
                 attention_mask=inputs["attention_mask"],
             )
+            encoder_output = outputs.last_hidden_state
 
-        return outputs.last_hidden_state  # (b, seq_len, h)
+        return encoder_output  # (b, seq_len, h)
