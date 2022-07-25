ace48022aad830eb53c8dd09ef94df3402e04525
Taka008 <kodama@nlp.ist.i.kyoto-u.ac.jp>
2022-07-25 13:24:37 +0900

    fix comment

--- tests/callbacks/test_typo_corrector_writer.py
+++ tests/callbacks/test_typo_corrector_writer.py
@@ -30,27 +30,29 @@ def test_write_on_epoch_end():
         )
 
         # tokenizer = writer.tokenizer
-        text = ["今日が晴れ"]
+        text = ["今日がも晴れ"]
 
-        kdr_logits = torch.zeros(1, 8, len(writer.opn2id), dtype=torch.float)
-        kdr_logits[0][0][writer.opn2id[TYPO_OPN2TOKEN["K"]]] = 1.0  # [CLS]
-        kdr_logits[0][1][writer.opn2id[TYPO_OPN2TOKEN["K"]]] = 1.0  # 今
-        kdr_logits[0][2][writer.opn2id[TYPO_OPN2TOKEN["K"]]] = 1.0  # 日
-        kdr_logits[0][3][writer.opn2id["は"]] = 1.0  # が -> は
-        kdr_logits[0][4][writer.opn2id[TYPO_OPN2TOKEN["K"]]] = 1.0  # 晴
-        kdr_logits[0][5][writer.opn2id[TYPO_OPN2TOKEN["K"]]] = 1.0  # れ
-        kdr_logits[0][6][writer.opn2id[TYPO_OPN2TOKEN["K"]]] = 1.0  # <dummy>
-        kdr_logits[0][7][writer.opn2id[TYPO_OPN2TOKEN["K"]]] = 1.0  # [SEP]
+        kdr_logits = torch.zeros(1, 9, len(writer.opn2id), dtype=torch.float)
+        kdr_logits[0][0][writer.opn2id[TYPO_OPN2TOKEN["K"]]] = 1.0  # keep: [CLS]
+        kdr_logits[0][1][writer.opn2id[TYPO_OPN2TOKEN["K"]]] = 1.0  # keep: 今
+        kdr_logits[0][2][writer.opn2id[TYPO_OPN2TOKEN["K"]]] = 1.0  # keep: 日
+        kdr_logits[0][3][writer.opn2id["は"]] = 1.0  # replace: が -> は
+        kdr_logits[0][4][writer.opn2id[TYPO_OPN2TOKEN["D"]]] = 1.0  # delete: も ->
+        kdr_logits[0][5][writer.opn2id[TYPO_OPN2TOKEN["K"]]] = 1.0  # keep: 晴
+        kdr_logits[0][6][writer.opn2id[TYPO_OPN2TOKEN["K"]]] = 1.0  # keep: れ
+        kdr_logits[0][7][writer.opn2id[TYPO_OPN2TOKEN["K"]]] = 1.0  # insert: <dummy> -> だ
+        kdr_logits[0][8][writer.opn2id[TYPO_OPN2TOKEN["K"]]] = 1.0  # keep: [SEP]
 
-        ins_logits = torch.zeros(1, 8, len(writer.opn2id), dtype=torch.float)
-        ins_logits[0][0][writer.opn2id[TYPO_OPN2TOKEN["_"]]] = 1.0  # [CLS]
-        ins_logits[0][1][writer.opn2id[TYPO_OPN2TOKEN["_"]]] = 1.0  # 今
-        ins_logits[0][2][writer.opn2id[TYPO_OPN2TOKEN["_"]]] = 1.0  # 日
-        ins_logits[0][3][writer.opn2id[TYPO_OPN2TOKEN["_"]]] = 1.0  # が -> は
-        ins_logits[0][4][writer.opn2id[TYPO_OPN2TOKEN["_"]]] = 1.0  # 晴
-        ins_logits[0][5][writer.opn2id[TYPO_OPN2TOKEN["_"]]] = 1.0  # れ
-        ins_logits[0][6][writer.opn2id["だ"]] = 1.0  # <dummy>
-        ins_logits[0][7][writer.opn2id[TYPO_OPN2TOKEN["_"]]] = 1.0  # [SEP]
+        ins_logits = torch.zeros(1, 9, len(writer.opn2id), dtype=torch.float)
+        ins_logits[0][0][writer.opn2id[TYPO_OPN2TOKEN["_"]]] = 1.0  # keep: [CLS]
+        ins_logits[0][1][writer.opn2id[TYPO_OPN2TOKEN["_"]]] = 1.0  # keep: 今
+        ins_logits[0][2][writer.opn2id[TYPO_OPN2TOKEN["_"]]] = 1.0  # keep: 日
+        ins_logits[0][3][writer.opn2id[TYPO_OPN2TOKEN["_"]]] = 1.0  # replace: が -> は
+        ins_logits[0][4][writer.opn2id[TYPO_OPN2TOKEN["_"]]] = 1.0  # delete: も ->
+        ins_logits[0][5][writer.opn2id[TYPO_OPN2TOKEN["_"]]] = 1.0  # keep: 晴
+        ins_logits[0][6][writer.opn2id[TYPO_OPN2TOKEN["_"]]] = 1.0  # keep: れ
+        ins_logits[0][7][writer.opn2id["だ"]] = 1.0  # insert: <dummy> -> だ
+        ins_logits[0][8][writer.opn2id[TYPO_OPN2TOKEN["_"]]] = 1.0  # keep: [SEP]
 
         predictions = [
             [
