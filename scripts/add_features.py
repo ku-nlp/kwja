358dbd28e416fe10e653c1dd70a8bf3e0d39249b
omura <omura@nlp.ist.i.kyoto-u.ac.jp>
2022-08-05 16:03:11 +0900

    fix bug

--- scripts/add_features.py
+++ scripts/add_features.py
@@ -65,9 +65,9 @@ class JumanppAugmenter:
             # Jumanpp may override reading
             augmented_morpheme._attributes.reading = original_morpheme.reading
             if update_original and not original_sentence.need_knp:
-                # convert Semantics to Features
+                # add Semantics
                 for k, v in augmented_morpheme.semantics.items():
-                    original_morpheme.features[k] = v
+                    original_morpheme.semantics[k] = v
 
 
 def add_features(doc_ids: list[str], reader: KyotoReader, output_dir: Path) -> None:
@@ -85,26 +85,27 @@ def add_features(doc_ids: list[str], reader: KyotoReader, output_dir: Path) -> N
                 old_knp_lines[idx] = line.replace(features, "")
                 buf.append((idx, features))
 
+        document = Document.from_knp("\n".join(old_knp_lines))
+        # 形態素意味情報付与 (引数に渡したdocumentをupdateする)
+        _ = jumanpp_augmenter.augment_document(document)
+
         # 素性付与
         with Popen(knp.run_command, stdout=PIPE, stdin=PIPE, encoding="utf-8", errors="replace") as p:
-            knp_text, _ = p.communicate(input="\n".join(old_knp_lines))
+            knp_text, _ = p.communicate(input=document.to_knp())
 
         new_knp_lines = knp_text.split("\n")
         # ダ列文語連体形など
         if len(new_knp_lines) != len(old_knp_lines):
             continue
 
+        # 初めから付いていた素性の付与
         for idx, features in buf:
             assert new_knp_lines[idx].endswith(">")
             new_knp_lines[idx] += features
         knp_text = "\n".join(new_knp_lines)
-        document = Document.from_knp(knp_text)
-
-        # 形態素意味情報付与 (引数に渡したdocumentをupdateする)
-        _ = jumanpp_augmenter.augment_document(document)
 
         with output_dir.joinpath(f"{document.doc_id}.knp").open(mode="w") as f:
-            f.write(document.to_knp())
+            f.write(knp_text)
 
 
 def test_jumanpp_augmenter():
@@ -137,18 +138,18 @@ def test_jumanpp_augmenter():
         # S-ID:w201106-0000060050-1 JUMAN:6.1-20101108 KNP:3.1-20101107 DATE:2011/06/21 SCORE:-44.94406 MOD:2017/10/15 MEMO:
         * 2D
         + 1D
-        コイン こいん コイン 名詞 6 普通名詞 1 * 0 * 0 <自動獲得:Wikipedia><Wikipediaリダイレクト:硬貨>
+        コイン こいん コイン 名詞 6 普通名詞 1 * 0 * 0 "自動獲得:Wikipedia Wikipediaリダイレクト:硬貨"
         + 3D <rel type="ガ" target="不特定:人"/><rel type="ヲ" target="コイン" sid="w201106-0000060050-1" id="0"/>
-        トス とす トス 名詞 6 サ変名詞 2 * 0 * 0 <代表表記:トス/とす><ドメイン:スポーツ><カテゴリ:抽象物>
-        を を を 助詞 9 格助詞 1 * 0 * 0
+        トス とす トス 名詞 6 サ変名詞 2 * 0 * 0 "代表表記:トス/とす ドメイン:スポーツ カテゴリ:抽象物"
+        を を を 助詞 9 格助詞 1 * 0 * 0 "代表表記:を/を"
         * 2D
         + 3D
-        ３ さん ３ 名詞 6 数詞 7 * 0 * 0 <カテゴリ:数量>
-        回 かい 回 接尾辞 14 名詞性名詞助数辞 3 * 0 * 0 <代表表記:回/かい><準内容語>
+        ３ さん ３ 名詞 6 数詞 7 * 0 * 0 "代表表記:３/さん カテゴリ:数量"
+        回 かい 回 接尾辞 14 名詞性名詞助数辞 3 * 0 * 0 "代表表記:回/かい 準内容語"
         * -1D
         + -1D <rel type="ガ" target="不特定:人"/><rel type="ガ" mode="？" target="読者"/><rel type="ガ" mode="？" target="著者"/><rel type="ヲ" target="トス" sid="w201106-0000060050-1" id="1"/>
-        行う おこなう 行う 動詞 2 * 0 子音動詞ワ行 12 基本形 2 <代表表記:行う/おこなう>
-        。 。 。 特殊 1 句点 1 * 0 * 0
+        行う おこなう 行う 動詞 2 * 0 子音動詞ワ行 12 基本形 2 "代表表記:行う/おこなう"
+        。 。 。 特殊 1 句点 1 * 0 * 0 "代表表記:。/。"
         EOS
         """
     )
@@ -209,46 +210,46 @@ def test_jumanpp_augmenter():
         # S-ID:w201106-0000060050-1 JUMAN:6.1-20101108 KNP:3.1-20101107 DATE:2011/06/21 SCORE:-44.94406 MOD:2017/10/15 MEMO:
         * 2D
         + 1D
-        コイン こいん コイン 名詞 6 普通名詞 1 * 0 * 0 <自動獲得:Wikipedia><Wikipediaリダイレクト:硬貨>
+        コイン こいん コイン 名詞 6 普通名詞 1 * 0 * 0 "自動獲得:Wikipedia Wikipediaリダイレクト:硬貨"
         + 3D <rel type="ガ" target="不特定:人"/><rel type="ヲ" target="コイン" sid="w201106-0000060050-1" id="0"/>
-        トス とす トス 名詞 6 サ変名詞 2 * 0 * 0 <代表表記:トス/とす><ドメイン:スポーツ><カテゴリ:抽象物>
-        を を を 助詞 9 格助詞 1 * 0 * 0
+        トス とす トス 名詞 6 サ変名詞 2 * 0 * 0 "代表表記:トス/とす ドメイン:スポーツ カテゴリ:抽象物"
+        を を を 助詞 9 格助詞 1 * 0 * 0 "代表表記:を/を"
         * 2D
         + 3D
-        ３ さん ３ 名詞 6 数詞 7 * 0 * 0 <カテゴリ:数量>
-        回 かい 回 接尾辞 14 名詞性名詞助数辞 3 * 0 * 0 <代表表記:回/かい><準内容語>
+        ３ さん ３ 名詞 6 数詞 7 * 0 * 0 "代表表記:３/さん カテゴリ:数量"
+        回 かい 回 接尾辞 14 名詞性名詞助数辞 3 * 0 * 0 "代表表記:回/かい 準内容語"
         * -1D
         + -1D <rel type="ガ" target="不特定:人"/><rel type="ガ" mode="？" target="読者"/><rel type="ガ" mode="？" target="著者"/><rel type="ヲ" target="トス" sid="w201106-0000060050-1" id="1"/>
-        行う おこなう 行う 動詞 2 * 0 子音動詞ワ行 12 基本形 2 <代表表記:行う/おこなう>
-        。 。 。 特殊 1 句点 1 * 0 * 0
+        行う おこなう 行う 動詞 2 * 0 子音動詞ワ行 12 基本形 2 "代表表記:行う/おこなう"
+        。 。 。 特殊 1 句点 1 * 0 * 0 "代表表記:。/。"
         EOS
         # S-ID:w201106-0000060050-2 JUMAN:6.1-20101108 KNP:3.1-20101107 DATE:2011/06/21 SCORE:-64.95916 MOD:2013/04/13
         * 1D
         + 1D <rel type="ノ" target="コイン" sid="w201106-0000060050-1" id="0"/>
-        表 おもて 表 名詞 6 普通名詞 1 * 0 * 0 <代表表記:表/おもて><カテゴリ:場所-機能><漢字読み:訓>
-        が が が 助詞 9 格助詞 1 * 0 * 0
+        表 おもて 表 名詞 6 普通名詞 1 * 0 * 0 "代表表記:表/おもて カテゴリ:場所-機能 漢字読み:訓"
+        が が が 助詞 9 格助詞 1 * 0 * 0 "代表表記:が/が"
         * 2D
         + 2D <rel type="ガ" target="表" sid="w201106-0000060050-2" id="0"/><rel type="外の関係" target="数" sid="w201106-0000060050-2" id="2"/>
-        出た でた 出る 動詞 2 * 0 母音動詞 1 タ形 10 <代表表記:出る/でる><反義:動詞:入る/はいる><自他動詞:他:出す/だす><補文ト>
+        出た でた 出る 動詞 2 * 0 母音動詞 1 タ形 10 "代表表記:出る/でる 反義:動詞:入る/はいる 自他動詞:他:出す/だす 補文ト"
         * 5D
         + 5D <rel type="ノ" target="出た" sid="w201106-0000060050-2" id="1"/>
-        数 かず 数 名詞 6 普通名詞 1 * 0 * 0 <代表表記:数/かず><カテゴリ:数量><漢字読み:訓>
-        だけ だけ だけ 助詞 9 副助詞 2 * 0 * 0
-        、 、 、 特殊 1 読点 2 * 0 * 0
+        数 かず 数 名詞 6 普通名詞 1 * 0 * 0 "代表表記:数/かず カテゴリ:数量 漢字読み:訓"
+        だけ だけ だけ 助詞 9 副助詞 2 * 0 * 0 "代表表記:だけ/だけ"
+        、 、 、 特殊 1 読点 2 * 0 * 0 "代表表記:、/、"
         * 4D
         + 4D
-        フィールド ふぃーるど フィールド 名詞 6 普通名詞 1 * 0 * 0 <代表表記:フィールド/ふぃーるど><カテゴリ:場所-その他>
-        上 じょう 上 接尾辞 14 名詞性名詞接尾辞 2 * 0 * 0 <代表表記:上/じょう>
-        の の の 助詞 9 接続助詞 3 * 0 * 0
+        フィールド ふぃーるど フィールド 名詞 6 普通名詞 1 * 0 * 0 "代表表記:フィールド/ふぃーるど カテゴリ:場所-その他"
+        上 じょう 上 接尾辞 14 名詞性名詞接尾辞 2 * 0 * 0 "代表表記:上/じょう"
+        の の の 助詞 9 接続助詞 3 * 0 * 0 "代表表記:の/の"
         * 5D
         + 5D <rel type="修飾" target="フィールド上" sid="w201106-0000060050-2" id="3"/><rel type="修飾" mode="AND" target="数" sid="w201106-0000060050-2" id="2"/>
-        モンスター もんすたー モンスター 名詞 6 普通名詞 1 * 0 * 0 <代表表記:モンスター/もんすたー><カテゴリ:人>
-        を を を 助詞 9 格助詞 1 * 0 * 0
+        モンスター もんすたー モンスター 名詞 6 普通名詞 1 * 0 * 0 "代表表記:モンスター/もんすたー カテゴリ:人"
+        を を を 助詞 9 格助詞 1 * 0 * 0 "代表表記:を/を"
         * -1D
         + -1D <rel type="ヲ" target="モンスター" sid="w201106-0000060050-2" id="4"/><rel type="ガ" target="不特定:状況"/>
-        破壊 はかい 破壊 名詞 6 サ変名詞 2 * 0 * 0 <代表表記:破壊/はかい><カテゴリ:抽象物><反義:名詞-サ変名詞:建設/けんせつ>
-        する する する 動詞 2 * 0 サ変動詞 16 基本形 2 <代表表記:する/する><自他動詞:自:成る/なる><付属動詞候補（基本）>
-        。 。 。 特殊 1 句点 1 * 0 * 0
+        破壊 はかい 破壊 名詞 6 サ変名詞 2 * 0 * 0 "代表表記:破壊/はかい カテゴリ:抽象物 反義:名詞-サ変名詞:建設/けんせつ"
+        する する する 動詞 2 * 0 サ変動詞 16 基本形 2 "代表表記:する/する 自他動詞:自:成る/なる 付属動詞候補（基本）"
+        。 。 。 特殊 1 句点 1 * 0 * 0 "代表表記:。/。"
         EOS
         """
     )
