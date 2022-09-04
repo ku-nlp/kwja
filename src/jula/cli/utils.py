91f589bdc4e98c4d2a91df99192d168f559c848d
nobu-g <ueda-n127@hotmail.co.jp>
2022-09-04 14:21:33 +0900

    suppress debug info from rhoknp logger

--- src/jula/cli/utils.py
+++ src/jula/cli/utils.py
@@ -7,6 +7,7 @@ import transformers.utils.logging as hf_logging
 def suppress_debug_info() -> None:
     warnings.filterwarnings("ignore")
     logging.getLogger("jula").setLevel(logging.ERROR)
+    logging.getLogger("rhoknp").setLevel(logging.ERROR)
     logging.getLogger("torch").setLevel(logging.ERROR)
     logging.getLogger("pytorch_lightning").setLevel(logging.ERROR)
     hf_logging.set_verbosity(hf_logging.ERROR)
