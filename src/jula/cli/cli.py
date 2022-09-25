4f8f6e038a32db1f0aa92e85c05b3477a57ad8ae
Taka008 <kodama@nlp.ist.i.kyoto-u.ac.jp>
2022-09-25 09:24:28 +0900

    add option to use additional model for discourse relation analysis

--- src/jula/cli/cli.py
+++ src/jula/cli/cli.py
@@ -9,6 +9,7 @@ import typer
 from dotenv import load_dotenv
 from omegaconf import OmegaConf
 from pytorch_lightning.trainer.states import TrainerFn
+from rhoknp import Document
 
 from jula.cli.utils import suppress_debug_info
 from jula.datamodule.datamodule import DataModule
@@ -26,6 +27,9 @@ app = typer.Typer()
 def main(
     text: Optional[str] = typer.Option(None, help="Text to be analyzed."),
     filename: Optional[Path] = typer.Option(None, help="File to be analyzed."),
+    discourse: Optional[bool] = typer.Option(
+        False, help="Whether to use a single model for discourse relation analysis"
+    ),
 ) -> None:
     if text is not None and filename is not None:
         typer.echo("ERROR: Please provide text or filename, not both")
@@ -47,8 +51,12 @@ def main(
     model_dir: Path = Path(env_model_dir)
 
     tmp_dir: TemporaryDirectory = TemporaryDirectory()
+    typo_path: Path = tmp_dir.name / Path("predict_typo.txt")
+    char_path: Path = tmp_dir.name / Path("predict_char.txt")
+    word_path: Path = tmp_dir.name / Path("predict_word.knp")
+    word_discourse_path: Path = tmp_dir.name / Path("predict_word_discourse.knp")
 
-    # typo
+    # typo module
     typo_model: TypoModule = TypoModule.load_from_checkpoint(str((model_dir / "typo.ckpt").resolve()))
     typo_cfg = typo_model.hparams
     typo_trainer: pl.Trainer = pl.Trainer(
@@ -58,7 +66,7 @@ def main(
             hydra.utils.instantiate(
                 typo_cfg.callbacks.prediction_writer,
                 output_dir=str(tmp_dir.name),
-                pred_filename="predict_typo",
+                pred_filename=typo_path.stem,
             )
         ],
         devices=1,
@@ -69,7 +77,7 @@ def main(
     typo_trainer.predict(model=typo_model, dataloaders=typo_datamodule.predict_dataloader())
     del typo_model
 
-    # char
+    # char module
     char_model: CharModule = CharModule.load_from_checkpoint(str((model_dir / "char.ckpt").resolve()))
     char_cfg = char_model.hparams
     char_trainer: pl.Trainer = pl.Trainer(
@@ -79,20 +87,18 @@ def main(
             hydra.utils.instantiate(
                 char_cfg.callbacks.prediction_writer,
                 output_dir=str(tmp_dir.name),
-                pred_filename="predict_char",
+                pred_filename=char_path.stem,
             )
         ],
         devices=1,
     )
-    with open(f"{tmp_dir.name}/predict_typo.txt") as f:
-        typo_results = [line.strip() for line in f]
-    char_cfg.datamodule.predict.texts = typo_results
+    char_cfg.datamodule.predict.texts = typo_path.read_text().splitlines()
     char_datamodule = DataModule(cfg=char_cfg.datamodule)
     char_datamodule.setup(stage=TrainerFn.PREDICTING)
     char_trainer.predict(model=char_model, dataloaders=char_datamodule.predict_dataloader())
     del char_model
 
-    # word
+    # word module
     word_model: WordModule = WordModule.load_from_checkpoint(str((model_dir / "word.ckpt").resolve()))
     word_cfg = word_model.hparams
     word_trainer: pl.Trainer = pl.Trainer(
@@ -102,16 +108,49 @@ def main(
             hydra.utils.instantiate(
                 word_cfg.callbacks.prediction_writer,
                 output_dir=str(tmp_dir.name),
-                pred_filename="predict_word",
-                use_stdout=True,
+                pred_filename=word_path.stem,
             )
         ],
         devices=1,
     )
-    with open(f"{tmp_dir.name}/predict_char.txt") as f:
-        char_results = [line.strip() for line in f]
+    char_results: list[str] = char_path.read_text().splitlines()
     word_cfg.datamodule.predict.texts = [x for i, x in enumerate(char_results) if i % 2 == 1]
     word_datamodule = DataModule(cfg=word_cfg.datamodule)
     word_datamodule.setup(stage=TrainerFn.PREDICTING)
     word_trainer.predict(model=word_model, dataloaders=word_datamodule.predict_dataloader())
+    del word_model
+    document: Document = Document.from_knp(word_path.read_text())
+    if not discourse:
+        print(document.to_knp())
+    else:
+        # word module (discourse)
+        word_discourse_model: WordModule = WordModule.load_from_checkpoint(
+            str((model_dir / "word_discourse.ckpt").resolve())
+        )
+        word_discourse_cfg = word_discourse_model.hparams
+        word_discourse_trainer: pl.Trainer = pl.Trainer(
+            logger=None,
+            enable_progress_bar=False,
+            callbacks=[
+                hydra.utils.instantiate(
+                    word_discourse_cfg.callbacks.prediction_writer,
+                    output_dir=str(tmp_dir.name),
+                    pred_filename=word_discourse_path.stem,
+                )
+            ],
+            devices=1,
+        )
+        word_discourse_cfg.datamodule.predict.texts = [x for i, x in enumerate(char_results) if i % 2 == 1]
+        word_discourse_datamodule = DataModule(cfg=word_discourse_cfg.datamodule)
+        word_discourse_datamodule.setup(stage=TrainerFn.PREDICTING)
+        word_discourse_trainer.predict(
+            model=word_discourse_model,
+            dataloaders=word_discourse_datamodule.predict_dataloader(),
+        )
+        discourse_document: Document = Document.from_knp(word_discourse_path.read_text())
+        for base_phrase in document.base_phrases:
+            base_phrase.discourse_relation_tag = discourse_document.base_phrases[
+                base_phrase.index
+            ].discourse_relation_tag
+        print(document.to_knp())
     tmp_dir.cleanup()
