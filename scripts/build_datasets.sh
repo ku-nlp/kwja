4e4fa5ed9e77d21d0272043a7ac8ee76a5c00d0d
omura <omura@nlp.ist.i.kyoto-u.ac.jp>
2022-10-30 00:32:01 +0900

    simplify build_datasets.sh

--- scripts/build_datasets.sh
+++ scripts/build_datasets.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/zsh
 
-while getopts a:w:s:j:o: OPT
+while getopts w:j:o: OPT
 do
   case $OPT in
-    a) ACTIVATOR="$OPTARG" ;;
     w) WORK_DIR="$OPTARG" ;;
-    s) SCRIPTS="$OPTARG" ;;
     j) JOBS="$OPTARG" ;;
     o) OUT_DIR="$OPTARG" ;;
     *)
@@ -14,14 +12,12 @@ do
   esac
 done
 
-# shellcheck source=/dev/null
-source "$ACTIVATOR"
 mkdir -p "$WORK_DIR" "$OUT_DIR"/{kwdlc,fuman}
 git clone git@github.com:ku-nlp/KWDLC.git "$WORK_DIR"/KWDLC
 git clone git@github.com:ku-nlp/AnnotatedFKCCorpus.git "$WORK_DIR"/AnnotatedFKCCorpus
-python "$SCRIPTS"/build_dataset.py "$WORK_DIR"/KWDLC/knp "$OUT_DIR"/kwdlc \
+poetry run python ./scripts/build_dataset.py "$WORK_DIR"/KWDLC/knp "$OUT_DIR"/kwdlc \
   --id "$WORK_DIR"/KWDLC/id/split_for_pas \
   -j "$JOBS"
-python "$SCRIPTS"/build_dataset.py "$WORK_DIR"/AnnotatedFKCCorpus/knp "$OUT_DIR"/fuman \
+poetry run python ./scripts/build_dataset.py "$WORK_DIR"/AnnotatedFKCCorpus/knp "$OUT_DIR"/fuman \
   --id "$WORK_DIR"/AnnotatedFKCCorpus/id/split_for_pas \
   -j "$JOBS"
