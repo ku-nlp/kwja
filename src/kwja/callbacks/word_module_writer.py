13eb0e47fac2bf004f474463a6ad7af102858d04
omura <omura@nlp.ist.i.kyoto-u.ac.jp>
2022-11-04 12:02:29 +0900

    fix is_target_sentence

--- src/kwja/callbacks/word_module_writer.py
+++ src/kwja/callbacks/word_module_writer.py
@@ -592,7 +592,6 @@ class WordModuleWriter(BasePredictionWriter):
                 conjform_preds,
             )
             document = self._chunk_morphemes(document, morphemes, word_feature_logits)
-            document.doc_id = doc_id
             self._add_base_phrase_features(document, base_phrase_feature_logits)
             self._add_named_entities(document, ne_tag_preds)
             self._add_dependency(document, dependency_preds, dependency_type_preds, dataset.special_to_index)
