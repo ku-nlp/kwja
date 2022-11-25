7d24417486d4a10b2f41ca1a1cb67991be1f2f46
nobu-g <ueda-n127@hotmail.co.jp>
2022-11-25 20:56:28 +0900

    add _rebuild_split_documents method

--- src/kwja/evaluators/char_module_metric.py
+++ src/kwja/evaluators/char_module_metric.py
@@ -1,9 +1,13 @@
-from typing import Dict, List, Union
+from collections import defaultdict
+from typing import Dict, Iterator, List, Union
 
-from rhoknp import Document
+from rhoknp import Document, Sentence
 from seqeval.metrics import accuracy_score, f1_score
 from seqeval.scheme import IOB2
 from torchmetrics import Metric
+
+from kwja.datamodule.datasets import CharDataset
+from kwja.utils.sub_document import extract_target_sentences, to_orig_doc_id
 
 
 class CharModuleMetric(Metric):
@@ -13,18 +17,25 @@ class CharModuleMetric(Metric):
         super().__init__()
         self.add_state("predicted_texts", default=[], dist_reduce_fx="cat")
         self.add_state("gold_texts", default=[], dist_reduce_fx="cat")
+        self.add_state("example_ids", default=[], dist_reduce_fx="cat")
 
-    def update(self, predicted_texts: List[str], gold_texts: List[str]) -> None:
+    def update(self, predicted_texts: List[str], gold_texts: List[str], example_ids: List[int]) -> None:
         """Update the internal state of the metric.
 
         Args:
             predicted_texts (List[str]): A list of predicted texts in the KNP format.
             gold_texts (List[str]): A list of gold texts in the KNP format.
+            example_ids (List[int]): A list of example IDs.
         """
         self.predicted_texts.append(predicted_texts)
         self.gold_texts.append(gold_texts)
+        self.example_ids.append(example_ids)
 
-    def compute(self) -> Dict[str, Union[str, float]]:
+    def compute(self, dataset: CharDataset) -> Dict[str, Union[str, float]]:
+        eid_to_did: Dict[int, str] = {e.example_id: e.doc_id for e in dataset.examples}
+        doc_ids: List[str] = [eid_to_did[eid] for eid in self.example_ids]
+        _ = self._rebuild_split_documents(self.predicted_texts, doc_ids)
+        _ = self._rebuild_split_documents(self.gold_texts, doc_ids)
         raise NotImplementedError
 
     def evaluate(self, predicted_text: str, gold_text: str) -> Dict[str, float]:
@@ -57,3 +68,16 @@ class CharModuleMetric(Metric):
                 zero_division="warn",
             ),
         }
+
+    @staticmethod
+    def _rebuild_split_documents(texts: List[str], doc_ids: List[str]) -> Iterator[Document]:
+        orig_did_to_sentences: Dict[str, List[Sentence]] = defaultdict(list)
+        for doc_id, text in zip(doc_ids, texts):
+            document = Document.from_jumanpp(text)
+            orig_doc_id = to_orig_doc_id(doc_id)
+            assert document.doc_id == orig_doc_id
+            document.doc_id = doc_id
+            orig_did_to_sentences[orig_doc_id] += extract_target_sentences(document)
+
+        for sentences in orig_did_to_sentences.values():
+            yield Document.from_sentences(sentences)
--- src/kwja/evaluators/word_module_metric.py
+++ src/kwja/evaluators/word_module_metric.py
@@ -1,6 +1,11 @@
-from typing import Dict, List
+from collections import defaultdict
+from typing import Dict, Iterator, List
 
+from rhoknp import Document, Sentence
 from torchmetrics import Metric
+
+from kwja.datamodule.datasets import WordDataset
+from kwja.utils.sub_document import extract_target_sentences, to_orig_doc_id
 
 
 class WordModuleMetric(Metric):
@@ -10,16 +15,36 @@ class WordModuleMetric(Metric):
         super().__init__()
         self.add_state("predicted_texts", default=[], dist_reduce_fx="cat")
         self.add_state("gold_texts", default=[], dist_reduce_fx="cat")
+        self.add_state("example_ids", default=[], dist_reduce_fx="cat")
 
-    def update(self, predicted_texts: List[str], gold_texts: List[str]) -> None:
+    def update(self, predicted_texts: List[str], gold_texts: List[str], example_ids: List[int]) -> None:
         """Update the internal state of the metric.
 
         Args:
             predicted_texts (List[str]): A list of predicted texts in the KNP format.
             gold_texts (List[str]): A list of gold texts in the KNP format.
+            example_ids (List[int]): A list of example IDs.
         """
         self.predicted_texts.append(predicted_texts)
         self.gold_texts.append(gold_texts)
+        self.example_ids.append(example_ids)
 
-    def compute(self) -> Dict[str, float]:
+    def compute(self, dataset: WordDataset) -> Dict[str, float]:
+        eid_to_did: Dict[int, str] = {e.example_id: e.doc_id for e in dataset.examples}
+        doc_ids: List[str] = [eid_to_did[eid] for eid in self.example_ids]
+        _ = self._rebuild_split_documents(self.predicted_texts, doc_ids)
+        _ = self._rebuild_split_documents(self.gold_texts, doc_ids)
         raise NotImplementedError
+
+    @staticmethod
+    def _rebuild_split_documents(texts: List[str], doc_ids: List[str]) -> Iterator[Document]:
+        orig_did_to_sentences: Dict[str, List[Sentence]] = defaultdict(list)
+        for doc_id, text in zip(doc_ids, texts):
+            document = Document.from_knp(text)
+            orig_doc_id = to_orig_doc_id(doc_id)
+            assert document.doc_id == orig_doc_id
+            document.doc_id = doc_id
+            orig_did_to_sentences[orig_doc_id] += extract_target_sentences(document)
+
+        for sentences in orig_did_to_sentences.values():
+            yield Document.from_sentences(sentences)
