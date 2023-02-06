dca141b083a358bc45fac567888474a70c8b94bc
omura <omura@nlp.ist.i.kyoto-u.ac.jp>
2023-02-06 13:47:01 +0900

    refactor typo dataset

--- src/kwja/datamodule/datasets/typo_dataset.py
+++ src/kwja/datamodule/datasets/typo_dataset.py
@@ -1,15 +1,16 @@
 import json
 from collections import defaultdict
 from pathlib import Path
-from typing import Dict, List, Union
+from typing import Dict, List, Optional, Union
 
 import torch
 from torch.utils.data import Dataset
 from transformers import AutoTokenizer, BatchEncoding, PreTrainedTokenizerBase
 from transformers.utils import PaddingStrategy
 
-from kwja.utils.constants import TYPO_DUMMY_TOKEN, TYPO_OPN2TOKEN
+from kwja.utils.constants import DUMMY_TOKEN, IGNORE_INDEX, TYPO_CORR_OP_TAG2TOKEN
 from kwja.utils.progress_bar import track
+from kwja.utils.typo_module_writer import get_maps
 
 
 class TypoDataset(Dataset):
@@ -18,89 +19,79 @@ class TypoDataset(Dataset):
         path: str,
         extended_vocab_path: str,
         model_name_or_path: str = "ku-nlp/roberta-base-japanese-char-wwm",
+        tokenizer_kwargs: Optional[dict] = None,
         max_seq_length: int = 512,
-        tokenizer_kwargs: dict = None,
     ) -> None:
         self.path = Path(path)
         assert self.path.is_dir()
-
-        self.documents = self.load_documents(self.path)
-        assert len(self) != 0
 
         self.tokenizer: PreTrainedTokenizerBase = AutoTokenizer.from_pretrained(
             model_name_or_path,
             **(tokenizer_kwargs or {}),
         )
-        assert self.tokenizer.pad_token_id is not None
-        self.pad_token_id: int = self.tokenizer.pad_token_id
         assert self.tokenizer.unk_token_id is not None
-        self.unk_token_id: int = self.tokenizer.unk_token_id
         self.max_seq_length: int = max_seq_length
 
-        self.opn2id: Dict[str, int] = self.get_opn2id(path=Path(extended_vocab_path))
+        self.token2token_id, self.token_id2token = get_maps(self.tokenizer, extended_vocab_path)
+
+        self.examples = self.load_examples(self.path)
         self.stash: Dict[int, List[str]] = defaultdict(list)
+        assert len(self) > 0
 
     def __len__(self) -> int:
-        return len(self.documents)
+        return len(self.examples)
 
     def __getitem__(self, index: int) -> Dict[str, Union[torch.Tensor, str]]:
         return self.encode(index)
 
     @staticmethod
-    def load_documents(path: Path) -> List[Dict[str, Union[str, List[str]]]]:
-        documents: List[Dict[str, Union[str, List[str]]]] = []
-        for file_path in track(sorted(path.glob("**/*.jsonl")), description="Loading documents"):
-            with file_path.open(mode="r", encoding="utf-8") as f:
+    def load_examples(example_dir: Path) -> List[Dict[str, Union[str, List[str]]]]:
+        examples: List[Dict[str, Union[str, List[str]]]] = []
+        for path in track(sorted(example_dir.glob("**/*.jsonl")), description="Loading documents"):
+            with path.open(mode="r", encoding="utf-8") as f:
                 for line in f:
-                    documents.append(json.loads(line))
-        return documents
-
-    def get_opn2id(self, path: Path) -> Dict[str, int]:
-        opn2id = self.tokenizer.get_vocab()
-        with path.open(mode="r") as f:
-            for line in f:
-                opn2id[str(line.strip())] = len(opn2id)
-        return opn2id
+                    examples.append(json.loads(line))
+        return examples
 
     def encode(self, example_id: int) -> Dict[str, Union[torch.Tensor, str]]:
-        document = self.documents[example_id]
-        if isinstance(document["pre_text"], list):
-            raise ValueError('document["pre_text"] must be string')
+        example = self.examples[example_id]
+
         encoding: BatchEncoding = self.tokenizer(
-            document["pre_text"] + TYPO_DUMMY_TOKEN,
+            example["pre_text"] + DUMMY_TOKEN,
             truncation=True,
             padding=PaddingStrategy.MAX_LENGTH,
             max_length=self.max_seq_length,
         )
-        input_ids = encoding["input_ids"]
-        attention_mask = encoding["attention_mask"]
 
         kdr_labels: List[int] = []
-        for opn in document["kdrs"][:-1]:
-            if opn in TYPO_OPN2TOKEN:
-                kdr_label = self.opn2id[TYPO_OPN2TOKEN[opn]]
+        for kdr_tag in example["kdrs"][:-1]:
+            if kdr_tag in TYPO_CORR_OP_TAG2TOKEN:
+                kdr_label = self.token2token_id[TYPO_CORR_OP_TAG2TOKEN[kdr_tag]]
             else:
-                kdr_label = self.opn2id.get(opn[2:], self.unk_token_id)  # remove prefix "R:" from opn
+                # remove prefix "R:" from kdr tag
+                kdr_label = self.token2token_id.get(kdr_tag[2:], self.tokenizer.unk_token_id)
             kdr_labels.append(kdr_label)
-        kdr_labels.append(self.pad_token_id)
-        kdr_labels = [self.pad_token_id] + kdr_labels[: self.max_seq_length - 2] + [self.pad_token_id]
-        kdr_labels = kdr_labels + [self.pad_token_id] * (self.max_seq_length - len(kdr_labels))
+        kdr_labels.append(IGNORE_INDEX)
+        # padding
+        kdr_labels = [IGNORE_INDEX] + kdr_labels[: self.max_seq_length - 2] + [IGNORE_INDEX]
+        kdr_labels += [IGNORE_INDEX] * (self.max_seq_length - len(kdr_labels))
 
         ins_labels: List[int] = []
-        for opn in document["inss"]:
-            if opn in TYPO_OPN2TOKEN:
-                ins_label = self.opn2id[TYPO_OPN2TOKEN[opn]]
+        for ins_tag in example["inss"]:
+            if ins_tag in TYPO_CORR_OP_TAG2TOKEN:
+                ins_label = self.token2token_id[TYPO_CORR_OP_TAG2TOKEN[ins_tag]]
             else:
-                ins_label = self.opn2id.get(opn[2:], self.unk_token_id)  # remove prefix "I:" from opn
+                # remove prefix "I:" from ins tag
+                ins_label = self.token2token_id.get(ins_tag[2:], self.tokenizer.unk_token_id)
             ins_labels.append(ins_label)
-        ins_labels = [self.pad_token_id] + ins_labels[: self.max_seq_length - 2] + [self.pad_token_id]
-        ins_labels = ins_labels + [self.pad_token_id] * (self.max_seq_length - len(ins_labels))
+        # padding
+        ins_labels = [IGNORE_INDEX] + ins_labels[: self.max_seq_length - 2] + [IGNORE_INDEX]
+        ins_labels += [IGNORE_INDEX] * (self.max_seq_length - len(ins_labels))
 
         return {
             "example_ids": torch.tensor(example_id, dtype=torch.long),
-            "input_ids": torch.tensor(input_ids, dtype=torch.long),
-            "attention_mask": torch.tensor(attention_mask, dtype=torch.long),
+            "input_ids": torch.tensor(encoding.input_ids, dtype=torch.long),
+            "attention_mask": torch.tensor(encoding.attention_mask, dtype=torch.long),
             "kdr_labels": torch.tensor(kdr_labels, dtype=torch.long),
             "ins_labels": torch.tensor(ins_labels, dtype=torch.long),
-            "texts": document["pre_text"],
         }
--- src/kwja/datamodule/datasets/typo_inference_dataset.py
+++ src/kwja/datamodule/datasets/typo_inference_dataset.py
@@ -1,22 +1,23 @@
 from collections import defaultdict
-from typing import Dict, List, Union
+from typing import Dict, List, Optional, Union
 
 import torch
+from omegaconf import ListConfig
 from torch.utils.data import Dataset
 from transformers import AutoTokenizer, BatchEncoding, PreTrainedTokenizerBase
 from transformers.utils import PaddingStrategy
 
-from kwja.utils.constants import TYPO_DUMMY_TOKEN
+from kwja.utils.constants import DUMMY_TOKEN
 from kwja.utils.progress_bar import track
 
 
 class TypoInferenceDataset(Dataset):
     def __init__(
         self,
-        texts: List[str],
+        texts: ListConfig,
         model_name_or_path: str = "ku-nlp/roberta-base-japanese-char-wwm",
+        tokenizer_kwargs: Optional[dict] = None,
         max_seq_length: int = 512,
-        tokenizer_kwargs: dict = None,
         **_,  # accept `extended_vocab_path` as a keyword argument
     ) -> None:
         self.tokenizer: PreTrainedTokenizerBase = AutoTokenizer.from_pretrained(
@@ -24,36 +25,35 @@ class TypoInferenceDataset(Dataset):
             **(tokenizer_kwargs or {}),
         )
         self.max_seq_length = max_seq_length
-        self.texts = []
+
+        self.examples: List[Dict[str, str]] = []
         self.stash: Dict[int, List[str]] = defaultdict(list)
         for text in track(texts, description="Loading documents"):
             text = text.strip()
             if len(self.tokenizer.tokenize(text)) == len(text) <= max_seq_length - 3:
-                self.texts.append(text)
+                self.examples.append({"pre_text": text})
             else:
-                self.stash[len(self.texts)].append(text)
-        if len(self.texts) == 0:
-            self.texts = [""]
+                self.stash[len(self.examples)].append(text)
+        if len(self.examples) == 0:
+            # len(self.examples) == 0だとwriterが呼ばれないのでダミーテキストを追加
+            self.examples = [{"pre_text": ""}]
 
     def __len__(self) -> int:
-        return len(self.texts)
+        return len(self.examples)
 
     def __getitem__(self, index: int) -> Dict[str, Union[torch.Tensor, str]]:
         return self.encode(index)
 
     def encode(self, example_id: int) -> Dict[str, Union[torch.Tensor, str]]:
-        text = self.texts[example_id]
+        example = self.examples[example_id]
         encoding: BatchEncoding = self.tokenizer(
-            text + TYPO_DUMMY_TOKEN,
+            example["pre_text"] + DUMMY_TOKEN,
             truncation=True,
             padding=PaddingStrategy.MAX_LENGTH,
             max_length=self.max_seq_length,
         )
-        input_ids = encoding["input_ids"]
-        attention_mask = encoding["attention_mask"]
         return {
             "example_ids": torch.tensor(example_id, dtype=torch.long),
-            "input_ids": torch.tensor(input_ids, dtype=torch.long),
-            "attention_mask": torch.tensor(attention_mask, dtype=torch.long),
-            "texts": text,
+            "input_ids": torch.tensor(encoding.input_ids, dtype=torch.long),
+            "attention_mask": torch.tensor(encoding.attention_mask, dtype=torch.long),
         }
