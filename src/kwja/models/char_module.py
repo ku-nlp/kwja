90ed8722dfb8fe792719600ec8539de81e93594a
omura <omura@nlp.ist.i.kyoto-u.ac.jp>
2023-02-06 14:00:55 +0900

    refactor char module

--- src/kwja/models/char_module.py
+++ src/kwja/models/char_module.py
@@ -1,19 +1,18 @@
 import copy
-from typing import Any, Dict, Optional
+from statistics import mean
+from typing import Any, Dict, List, Optional, Tuple
 
 import hydra
 import pytorch_lightning as pl
 import torch
 from omegaconf import DictConfig, OmegaConf
-from pytorch_lightning.utilities.types import STEP_OUTPUT
-from transformers import PretrainedConfig
+from transformers import PretrainedConfig, PreTrainedModel
 
-from kwja.evaluators.word_normalization_metric import WordNormalizationMetric
-from kwja.evaluators.word_segmentation_metric import WordSegmentationMetric
-from kwja.models.models.char_encoder import CharEncoder
-from kwja.models.models.word_normalizer import WordNormalizer
-from kwja.models.models.word_segmenter import WordSegmenter
-from kwja.utils.util import filter_dict_items
+from kwja.evaluators.char_module_metric import CharModuleMetric
+from kwja.models.components.head import SequenceLabelingHead
+from kwja.utils.constants import WORD_NORM_OP_TAGS, WORD_SEGMENTATION_TAGS
+from kwja.utils.loss import compute_sequence_mean_loss
+from kwja.utils.omegaconf import filter_dict_items
 
 
 class CharModule(pl.LightningModule):
@@ -21,130 +20,116 @@ class CharModule(pl.LightningModule):
         super().__init__()
         self.save_hyperparameters(hparams)
 
-        self.valid_corpora = list(hparams.datamodule.valid.keys()) if "valid" in hparams.datamodule else []
-        self.test_corpora = list(hparams.datamodule.test.keys()) if "test" in hparams.datamodule else []
+        if valid_corpora := getattr(hparams.datamodule, "valid", None):
+            self.valid_corpora: List[str] = list(valid_corpora)
+            self.valid_corpus2char_module_metric: Dict[str, CharModuleMetric] = {
+                corpus: CharModuleMetric() for corpus in self.valid_corpora
+            }
+        if test_corpora := getattr(hparams.datamodule, "test", None):
+            self.test_corpora: List[str] = list(test_corpora)
+            self.test_corpus2char_module_metric: Dict[str, CharModuleMetric] = {
+                corpus: CharModuleMetric() for corpus in self.test_corpora
+            }
 
-        self.char_encoder: CharEncoder = CharEncoder(hparams)
+        self.char_encoder: PreTrainedModel = hydra.utils.call(hparams.encoder)
+        pretrained_model_config: PretrainedConfig = self.char_encoder.config
+        if hasattr(hparams.dataset, "special_tokens"):
+            self.char_encoder.resize_token_embeddings(
+                pretrained_model_config.vocab_size + len(hparams.dataset.special_tokens)
+            )
+        head_args: Tuple[int, float] = (
+            pretrained_model_config.hidden_size,
+            pretrained_model_config.hidden_dropout_prob,
+        )
 
-        pretrained_model_config: PretrainedConfig = self.char_encoder.pretrained_model.config
+        # ---------- word segmentation ----------
+        self.word_segmentation_tagger = SequenceLabelingHead(len(WORD_SEGMENTATION_TAGS), *head_args)
 
-        self.word_segmenter = WordSegmenter(hparams, pretrained_model_config)
-        self.valid_word_segmenter_metrics: Dict[str, WordSegmentationMetric] = {
-            corpus: WordSegmentationMetric() for corpus in self.valid_corpora
-        }
-        self.test_word_segmenter_metrics: Dict[str, WordSegmentationMetric] = {
-            corpus: WordSegmentationMetric() for corpus in self.test_corpora
-        }
+        # ---------- word normalization ----------
+        self.word_norm_op_tagger = SequenceLabelingHead(len(WORD_NORM_OP_TAGS), *head_args)
 
-        self.word_normalizer = WordNormalizer(hparams, pretrained_model_config)
-        self.valid_word_normalizer_metrics: Dict[str, WordNormalizationMetric] = {
-            corpus: WordNormalizationMetric() for corpus in self.valid_corpora
-        }
-        self.test_word_normalizer_metrics: Dict[str, WordNormalizationMetric] = {
-            corpus: WordNormalizationMetric() for corpus in self.test_corpora
-        }
-
-    def forward(self, **kwargs) -> Dict[str, Dict[str, torch.Tensor]]:
-        encoder_output = self.char_encoder(kwargs)  # (b, seq_len, h)
-        word_segmenter_outputs = self.word_segmenter(encoder_output, kwargs)
-        word_normalizer_outputs = self.word_normalizer(encoder_output, kwargs)
+    def forward(self, batch: Any) -> Dict[str, torch.Tensor]:
+        self._truncate(batch)
+        encoded = self.char_encoder(input_ids=batch["input_ids"], attention_mask=batch["attention_mask"])
         return {
-            "word_segmenter_outputs": word_segmenter_outputs,
-            "word_normalizer_outputs": word_normalizer_outputs,
+            "word_segmentation_logits": self.word_segmentation_tagger(encoded.last_hidden_state),
+            "word_norm_op_logits": self.word_norm_op_tagger(encoded.last_hidden_state),
         }
 
-    def training_step(self, batch: Any, batch_idx: int) -> STEP_OUTPUT:
-        outputs: Dict[str, Dict[str, torch.Tensor]] = self(**batch)
-        word_segmenter_loss = outputs["word_segmenter_outputs"]["loss"]
-        self.log("train/word_segmenter_loss", word_segmenter_loss)
-        word_normalizer_loss = outputs["word_normalizer_outputs"]["loss"]
-        self.log("train/word_normalizer_loss", word_normalizer_loss)
-        return word_segmenter_loss + word_normalizer_loss
+    @staticmethod
+    def _truncate(batch: Any) -> None:
+        max_seq_length = batch["attention_mask"].sum(dim=1).max().item()
+        for key, value in batch.items():
+            if key in {"input_ids", "attention_mask", "word_segmentation_labels", "word_norm_op_labels"}:
+                batch[key] = value[:, :max_seq_length].contiguous()
 
-    def validation_step(self, batch: Any, batch_idx: int, dataloader_idx: Optional[int] = None) -> Any:
-        outputs: Dict[str, Dict[str, torch.Tensor]] = self(**batch)
+    def training_step(self, batch: Any, batch_idx: int) -> torch.Tensor:
+        ret: Dict[str, torch.Tensor] = self(batch)
+        word_segmentation_loss = compute_sequence_mean_loss(
+            ret["word_segmentation_logits"], batch["word_segmentation_labels"]
+        )
+        self.log("train/word_segmentation_loss", word_segmentation_loss)
+        word_normalization_loss = compute_sequence_mean_loss(ret["word_norm_op_logits"], batch["word_norm_op_labels"])
+        self.log("train/word_normalization_loss", word_normalization_loss)
+        return word_segmentation_loss + word_normalization_loss
+
+    def validation_step(self, batch: Any, batch_idx: int, dataloader_idx: Optional[int] = None) -> None:
+        metric_args = self.predict_step(batch, batch_idx, dataloader_idx=dataloader_idx)
+        metric_args.update({"word_norm_op_labels": batch["word_norm_op_labels"]})
         corpus = self.valid_corpora[dataloader_idx or 0]
-        word_segmenter_args = {
-            "seg_preds": torch.argmax(outputs["word_segmenter_outputs"]["logits"], dim=-1),
-            "seg_types": batch["seg_types"],
-        }
-        self.valid_word_segmenter_metrics[corpus].update(**word_segmenter_args)
-        self.log("valid/word_segmenter_loss", outputs["word_segmenter_outputs"]["loss"])
-        word_normalizer_args = {
-            "norm_preds": torch.argmax(outputs["word_normalizer_outputs"]["logits"], dim=-1),
-            "norm_types": batch["norm_types"],
-        }
-        self.valid_word_normalizer_metrics[corpus].update(**word_normalizer_args)
-        self.log("valid/word_normalizer_loss", outputs["word_normalizer_outputs"]["loss"])
+        dataset = self.trainer.val_dataloaders[dataloader_idx or 0].dataset
+        self.valid_corpus2char_module_metric[corpus].update(metric_args, dataset)
 
     def validation_epoch_end(self, validation_step_outputs) -> None:
-        word_segmenter_f1 = 0.0
-        for corpus, metric in self.valid_word_segmenter_metrics.items():
-            for name, value in metric.compute().items():
-                if name == "word_segmenter_f1":
-                    word_segmenter_f1 += value
-                self.log(f"valid_{corpus}/{name}", value)
-                metric.reset()
-        word_segmenter_f1 /= len(self.valid_word_segmenter_metrics)
-        self.log("valid/word_segmenter_f1", word_segmenter_f1)
+        metrics_log: Dict[str, Dict[str, float]] = {corpus: {} for corpus in self.valid_corpora}
+        for corpus, char_module_metric in self.valid_corpus2char_module_metric.items():
+            metrics = char_module_metric.compute()
+            metrics["aggregated_char_metrics"] = mean(
+                metrics[key] for key in ["word_segmentation_f1", "word_normalization_f1"] if key in metrics
+            )
+            metrics_log[corpus] = metrics
+            char_module_metric.reset()
 
-        word_normalizer_f1 = 0.0
-        for corpus, metric in self.valid_word_normalizer_metrics.items():
-            for name, value in metric.compute().items():
-                if name == "word_segmenter_f1":
-                    word_segmenter_f1 += value
-                self.log(f"valid_{corpus}/{name}", value)
-                metric.reset()
-        word_normalizer_f1 /= len(self.valid_word_normalizer_metrics)
-        self.log("valid/word_normalizer_f1", word_normalizer_f1)
+        for corpus, metrics in metrics_log.items():
+            self.log_dict({f"valid_{corpus}/{key}": value for key, value in metrics.items()})
+        for key in list(metrics_log.values())[0].keys():
+            self.log(
+                f"valid/{key}",
+                mean(metrics_log[corpus][key] for corpus in self.valid_corpora if key in metrics_log[corpus]),
+            )
 
-        self.log("valid/f1", (word_segmenter_f1 + word_normalizer_f1) / 2)
-
-    def test_step(self, batch: Any, batch_idx: int, dataloader_idx: Optional[int] = None) -> Any:
-        outputs: Dict[str, Dict[str, torch.Tensor]] = self(**batch)
+    def test_step(self, batch: Any, batch_idx: int, dataloader_idx: Optional[int] = None) -> None:
+        metric_args = self.predict_step(batch, batch_idx, dataloader_idx=dataloader_idx)
+        metric_args.update({"word_norm_op_labels": batch["word_norm_op_labels"]})
         corpus = self.test_corpora[dataloader_idx or 0]
-        word_segmenter_args = {
-            "seg_preds": torch.argmax(outputs["word_segmenter_outputs"]["logits"], dim=-1),
-            "seg_types": batch["seg_types"],
-        }
-        self.test_word_segmenter_metrics[corpus].update(**word_segmenter_args)
-        self.log("test/word_segmenter_loss", outputs["word_segmenter_outputs"]["loss"])
-        word_normalizer_args = {
-            "norm_preds": torch.argmax(outputs["word_normalizer_outputs"]["logits"], dim=-1),
-            "norm_types": batch["norm_types"],
-        }
-        self.test_word_normalizer_metrics[corpus].update(**word_normalizer_args)
-        self.log("test/word_normalizer_loss", outputs["word_normalizer_outputs"]["loss"])
+        dataset = self.trainer.test_dataloaders[dataloader_idx or 0].dataset
+        self.test_corpus2char_module_metric[corpus].update(metric_args, dataset)
 
     def test_epoch_end(self, test_step_outputs) -> None:
-        word_segmenter_f1 = 0.0
-        for corpus, metric in self.test_word_segmenter_metrics.items():
-            for name, value in metric.compute().items():
-                if name == "word_segmenter_f1":
-                    word_segmenter_f1 += value
-                self.log(f"test_{corpus}/{name}", value)
-                metric.reset()
-        word_segmenter_f1 /= len(self.test_word_segmenter_metrics)
-        self.log("test/word_segmenter_f1", word_segmenter_f1)
-        self.log("test/f1", word_segmenter_f1)
+        metrics_log: Dict[str, Dict[str, float]] = {corpus: {} for corpus in self.test_corpora}
+        for corpus, char_module_metric in self.test_corpus2char_module_metric.items():
+            metrics = char_module_metric.compute()
+            metrics["aggregated_char_metrics"] = mean(
+                metrics[key] for key in ["word_segmentation_f1", "word_normalization_f1"] if key in metrics
+            )
+            metrics_log[corpus] = metrics
+            char_module_metric.reset()
 
-        word_normalizer_f1 = 0.0
-        for corpus, metric in self.test_word_normalizer_metrics.items():
-            for name, value in metric.compute().items():
-                if name == "word_segmenter_f1":
-                    word_segmenter_f1 += value
-                self.log(f"test_{corpus}/{name}", value)
-                metric.reset()
-        word_normalizer_f1 /= len(self.test_word_normalizer_metrics)
-        self.log("valid/word_normalizer_f1", word_normalizer_f1)
+        for corpus, metrics in metrics_log.items():
+            self.log_dict({f"test_{corpus}/{key}": value for key, value in metrics.items()})
+        for key in list(metrics_log.values())[0].keys():
+            self.log(
+                f"test/{key}",
+                mean(metrics_log[corpus][key] for corpus in self.test_corpora if key in metrics_log[corpus]),
+            )
 
-    def predict_step(self, batch: Any, batch_idx: int, dataloader_idx: Optional[int] = None) -> Any:
-        outputs: Dict[str, Dict[str, torch.Tensor]] = self(**batch)
+    def predict_step(self, batch: Any, batch_idx: int, dataloader_idx: Optional[int] = None) -> Dict[str, torch.Tensor]:
+        ret: Dict[str, torch.Tensor] = self(batch)
         return {
             "example_ids": batch["example_ids"],
             "input_ids": batch["input_ids"],
-            "word_segmenter_logits": outputs["word_segmenter_outputs"]["logits"],
-            "word_normalizer_logits": outputs["word_normalizer_outputs"]["logits"],
+            "word_segmentation_predictions": ret["word_segmentation_logits"].argmax(dim=2),
+            "word_norm_op_predictions": ret["word_norm_op_logits"].argmax(dim=2),
         }
 
     def configure_optimizers(self):
