d80dcb30c50c0bf635720cde6051857bcef41103
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2023-02-16 16:41:00 +0900

    test loss.py

---
+++ tests/utils/test_loss.py
@@ -0,0 +1,53 @@
+import pytest
+import torch
+
+from kwja.utils.constants import MASKED
+from kwja.utils.loss import compute_multi_label_token_mean_loss, compute_token_mean_loss, mask_logits
+
+
+@pytest.mark.parametrize(
+    "input_, target, expected",
+    [
+        (
+            torch.tensor([[1.0, 0.0]], dtype=torch.float),  # (1, 2)
+            torch.tensor([[0]], dtype=torch.long),  # (1,)
+            torch.tensor(0.3132616875182228, dtype=torch.float),  # (,)
+        ),
+    ],
+)
+def test_compute_token_mean_loss(input_: torch.Tensor, target: torch.Tensor, expected: torch.Tensor) -> None:
+    assert torch.isclose(compute_token_mean_loss(input_, target), expected)
+
+
+@pytest.mark.parametrize(
+    "input_, target, mask, expected",
+    [
+        (
+            torch.tensor([[[0.5, 0.5], [1.0, 0.5]]], dtype=torch.float),  # (1, 2, 2)
+            torch.tensor([[[1.0, 0.0], [1.0, 0.0]]], dtype=torch.float),  # (1, 2, 2)
+            torch.tensor([[[True, True], [True, True]]], dtype=torch.bool),  # (1, 2, 2)
+            torch.tensor(1.0397207708399179, dtype=torch.float),
+        ),
+    ],
+)
+def test_compute_multi_label_token_mean_loss(
+    input_: torch.Tensor,
+    target: torch.Tensor,
+    mask: torch.Tensor,
+    expected: torch.Tensor,
+) -> None:
+    assert torch.isclose(compute_multi_label_token_mean_loss(input_, target, mask), expected)
+
+
+@pytest.mark.parametrize(
+    "logits, mask, expected",
+    [
+        (
+            torch.tensor([1.0, 1.0, 1.0], dtype=torch.float),  # (3,)
+            torch.tensor([True, False, False], dtype=torch.bool),  # (3,)
+            torch.tensor([1.0, MASKED, MASKED], dtype=torch.float),  # (3,)
+        )
+    ],
+)
+def test_mask_logits(logits: torch.Tensor, mask: torch.Tensor, expected: torch.Tensor) -> None:
+    assert torch.allclose(mask_logits(logits, mask), expected)
