15312414cff43fe4f1a5c371c5a178f27f2138c5
omura <omura@nlp.ist.i.kyoto-u.ac.jp>
2023-02-26 19:22:04 +0900

    implement test typo/char module metric

--- tests/evaluators/test_char_module_metric.py
+++ tests/evaluators/test_char_module_metric.py
@@ -1,73 +1,82 @@
-# import textwrap
+from pathlib import Path
 
-# from kwja.evaluators.char_module_metric import CharModuleMetric
+import torch
+from transformers import AutoTokenizer
 
-# TODO
-# def test_update() -> None:
-#     metric = CharModuleMetric()
-#     gold = textwrap.dedent(
-#         """\
-#         # S-ID:000-1
-#         風 _ 風 未定義語 15 その他 1 * 0 * 0
-#         が _ が 未定義語 15 その他 1 * 0 * 0
-#         吹く _ 吹く 未定義語 15 その他 1 * 0 * 0
-#         。 _ 。 未定義語 15 その他 1 * 0 * 0
-#         EOS
-#         """
-#     )
-#     predicted = gold
-#     gold = predicted
-#     metric.update([predicted], [gold], [0])
-#     metric.update([predicted], [gold], [1])
+from kwja.datamodule.datasets.char_dataset import CharDataset
+from kwja.evaluators.char_module_metric import CharModuleMetric
+from kwja.utils.constants import IGNORE_INDEX, WORD_NORM_OP_TAGS, WORD_SEGMENTATION_TAGS
 
 
-# def test_evaluate_word_segmentation_0() -> None:
-#     metric = CharModuleMetric()
-#     gold = textwrap.dedent(
-#         """\
-#         # S-ID:000-1
-#         風 _ 風 未定義語 15 その他 1 * 0 * 0
-#         が _ が 未定義語 15 その他 1 * 0 * 0
-#         吹く _ 吹く 未定義語 15 その他 1 * 0 * 0
-#         。 _ 。 未定義語 15 その他 1 * 0 * 0
-#         EOS
-#         """
-#     )
-#     predicted = gold
-#     actual = metric.evaluate_word_segmentation(predicted, gold)
-#     expected = {
-#         "word_segmentation/acc": 1.0,
-#         "word_segmentation/f1": 1.0,
-#     }
-#     assert actual == expected
+def test_char_module_metric() -> None:
+    metric = CharModuleMetric()
 
+    path = Path(__file__).absolute().parent.joinpath("char_files")
+    tokenizer = AutoTokenizer.from_pretrained("ku-nlp/roberta-base-japanese-char-wwm")
+    max_seq_length = 20
+    dataset = CharDataset(str(path), tokenizer, max_seq_length)  # denormalize_probability == 0.0
+    metric.set_properties(dataset)
 
-# def test_evaluate_word_segmentation_1() -> None:
-#     metric = CharModuleMetric()
-#     predicted = textwrap.dedent(
-#         """\
-#         # S-ID:000-1
-#         風 _ 風 未定義語 15 その他 1 * 0 * 0
-#         が _ が 未定義語 15 その他 1 * 0 * 0
-#         吹 _ 吹 未定義語 15 その他 1 * 0 * 0
-#         く _ く 未定義語 15 その他 1 * 0 * 0
-#         。 _ 。 未定義語 15 その他 1 * 0 * 0
-#         EOS
-#         """
-#     )
-#     gold = textwrap.dedent(
-#         """\
-#         # S-ID:000-1
-#         風 _ 風 未定義語 15 その他 1 * 0 * 0
-#         が _ が 未定義語 15 その他 1 * 0 * 0
-#         吹く _ 吹く 未定義語 15 その他 1 * 0 * 0
-#         。 _ 。 未定義語 15 その他 1 * 0 * 0
-#         EOS
-#         """
-#     )
-#     actual = metric.evaluate_word_segmentation(predicted, gold)
-#     expected = {
-#         "word_segmentation/acc": 4 / 5,
-#         "word_segmentation/f1": (2 * (3 / 5) * (3 / 4)) / (3 / 5 + 3 / 4),  # tp: 3, fp: 1, fn: 1
-#     }
-#     assert actual == expected
+    metric.example_ids = torch.arange(len(dataset), dtype=torch.long)
+
+    metric.word_segmentation_predictions = torch.full((len(dataset), max_seq_length), IGNORE_INDEX, dtype=torch.long)
+    # [:, 0] = [CLS]
+    metric.word_segmentation_predictions[0, 1] = WORD_SEGMENTATION_TAGS.index("B")  # 花
+    metric.word_segmentation_predictions[0, 2] = WORD_SEGMENTATION_TAGS.index("I")  # 咲
+    metric.word_segmentation_predictions[0, 3] = WORD_SEGMENTATION_TAGS.index("B")  # ガ
+    metric.word_segmentation_predictions[0, 4] = WORD_SEGMENTATION_TAGS.index("I")  # ニ
+    metric.word_segmentation_predictions[0, 5] = WORD_SEGMENTATION_TAGS.index("B")  # を
+    metric.word_segmentation_predictions[0, 6] = WORD_SEGMENTATION_TAGS.index("B")  # 買
+    metric.word_segmentation_predictions[0, 7] = WORD_SEGMENTATION_TAGS.index("I")  # ぅ
+    metric.word_segmentation_predictions[1, 1] = WORD_SEGMENTATION_TAGS.index("B")  # う
+    metric.word_segmentation_predictions[1, 2] = WORD_SEGMENTATION_TAGS.index("I")  # ま
+    metric.word_segmentation_predictions[1, 3] = WORD_SEGMENTATION_TAGS.index("I")  # そ (gold = "B")
+    metric.word_segmentation_predictions[1, 4] = WORD_SEGMENTATION_TAGS.index("I")  # ー
+    metric.word_segmentation_predictions[1, 5] = WORD_SEGMENTATION_TAGS.index("I")  # で
+    metric.word_segmentation_predictions[1, 6] = WORD_SEGMENTATION_TAGS.index("I")  # す
+    metric.word_segmentation_predictions[1, 7] = WORD_SEGMENTATION_TAGS.index("B")  # ね
+    metric.word_segmentation_predictions[1, 8] = WORD_SEGMENTATION_TAGS.index("I")  # 〜
+    metric.word_segmentation_predictions[1, 9] = WORD_SEGMENTATION_TAGS.index("I")  # 〜
+
+    metric.word_norm_op_predictions = torch.full((len(dataset), max_seq_length), IGNORE_INDEX, dtype=torch.long)
+    # [:, 0] = [CLS]
+    metric.word_norm_op_predictions[0, 1] = WORD_NORM_OP_TAGS.index("K")  # 花
+    metric.word_norm_op_predictions[0, 2] = WORD_NORM_OP_TAGS.index("K")  # 咲
+    metric.word_norm_op_predictions[0, 3] = WORD_NORM_OP_TAGS.index("K")  # ガ (gold = "V")
+    metric.word_norm_op_predictions[0, 4] = WORD_NORM_OP_TAGS.index("K")  # ニ
+    metric.word_norm_op_predictions[0, 5] = WORD_NORM_OP_TAGS.index("K")  # を
+    metric.word_norm_op_predictions[0, 6] = WORD_NORM_OP_TAGS.index("K")  # 買
+    metric.word_norm_op_predictions[0, 7] = WORD_NORM_OP_TAGS.index("S")  # ぅ
+    metric.word_norm_op_predictions[1, 1] = WORD_NORM_OP_TAGS.index("K")  # う
+    metric.word_norm_op_predictions[1, 2] = WORD_NORM_OP_TAGS.index("K")  # ま
+    metric.word_norm_op_predictions[1, 3] = WORD_NORM_OP_TAGS.index("K")  # そ
+    metric.word_norm_op_predictions[1, 4] = WORD_NORM_OP_TAGS.index("K")  # ー (gold = "P")
+    metric.word_norm_op_predictions[1, 5] = WORD_NORM_OP_TAGS.index("K")  # で
+    metric.word_norm_op_predictions[1, 6] = WORD_NORM_OP_TAGS.index("K")  # す
+    metric.word_norm_op_predictions[1, 7] = WORD_NORM_OP_TAGS.index("K")  # ね
+    metric.word_norm_op_predictions[1, 8] = WORD_NORM_OP_TAGS.index("E")  # 〜
+    metric.word_norm_op_predictions[1, 9] = WORD_NORM_OP_TAGS.index("D")  # 〜
+    metric.word_norm_op_labels = torch.stack([dataset[eid]["word_norm_op_labels"] for eid in metric.example_ids], dim=0)
+
+    digits = 4
+    metrics = {k: round(v, digits) for k, v in metric.compute().items()}
+
+    assert metrics["word_segmentation_accuracy"] == round(15 / 16, digits)
+    # tp = 5, fp = 1, fn = 2 (span-level)
+    assert metrics["word_segmentation_f1"] == round((2 * 5 / 6 * 5 / 7) / (5 / 6 + 5 / 7), digits)
+
+    assert metrics["word_normalization_accuracy"] == round(14 / 16, digits)
+    # tp = 3, fp = 2, fn = 0 (other than KEEP)
+    assert metrics["word_normalization_f1"] == round((2 * 3 / 5 * 3 / 3) / (3 / 5 + 3 / 3), digits)
+    # tp = 11, fp = 2, fn = 0
+    assert metrics["word_normalization_f1:K"] == round((2 * 11 / 13 * 11 / 11) / (11 / 13 + 11 / 11), digits)
+    # tp = 1, fp = 0, fn = 0
+    assert metrics["word_normalization_f1:D"] == round((2 * 1 / 1 * 1 / 1) / (1 / 1 + 1 / 1), digits)
+    # tp = 0, fp = 0, fn = 1
+    assert metrics["word_normalization_f1:V"] == 0.0
+    # tp = 1, fp = 0, fn = 0
+    assert metrics["word_normalization_f1:S"] == round((2 * 1 / 1 * 1 / 1) / (1 / 1 + 1 / 1), digits)
+    # tp = 0, fp = 0, fn = 1
+    assert metrics["word_normalization_f1:P"] == 0.0
+    # tp = 1, fp = 0, fn = 0
+    assert metrics["word_normalization_f1:E"] == round((2 * 1 / 1 * 1 / 1) / (1 / 1 + 1 / 1), digits)
--- tests/evaluators/test_typo_module_metric.py
+++ tests/evaluators/test_typo_module_metric.py
@@ -1,9 +1,69 @@
-# from kwja.evaluators.typo_module_metric import TypoModuleMetric
+from pathlib import Path
 
-# TODO
-# def test_typo_module_metric() -> None:
-#     metric = TypoModuleMetric()
-#     predicted = "風が吹く。"
-#     gold = predicted
-#     metric.update([predicted], [gold])
-#     metric.update([predicted], [gold])  # Add the same data twice.
+import torch
+from transformers import AutoTokenizer
+
+from kwja.datamodule.datasets.typo_dataset import TypoDataset
+from kwja.evaluators.typo_module_metric import TypoModuleMetric
+
+
+def test_typo_module_metric() -> None:
+    metric = TypoModuleMetric(confidence_thresholds=(0.0, 0.8, 0.9))
+
+    path = Path(__file__).absolute().parent.joinpath("typo_files")
+    tokenizer = AutoTokenizer.from_pretrained(
+        "ku-nlp/roberta-base-japanese-char-wwm",
+        do_word_tokenize=False,
+        additional_special_tokens=["<k>", "<d>", "<_>", "<dummy>"],
+    )
+    max_seq_length = 20
+    dataset = TypoDataset(str(path), tokenizer, max_seq_length=max_seq_length)
+    metric.set_properties(dataset)
+
+    metric.example_ids = torch.arange(len(dataset), dtype=torch.long)
+    kdr_probabilities = torch.zeros((2, max_seq_length, len(dataset.token2token_id)), dtype=torch.float)
+    kdr_probabilities[0, 0, dataset.token2token_id["<k>"]] = 1.0  # [CLS]
+    kdr_probabilities[0, 1, dataset.token2token_id["松"]] = 0.65  # 待 -> 松
+    kdr_probabilities[0, 2, dataset.token2token_id["<d>"]] = 0.85  # つ -> φ
+    kdr_probabilities[0, 3, dataset.token2token_id["<k>"]] = 1.0  # の
+    kdr_probabilities[0, 4, dataset.token2token_id["<k>"]] = 1.0  # 木
+    kdr_probabilities[0, 5, dataset.token2token_id["<k>"]] = 1.0  # が
+    kdr_probabilities[0, 6, dataset.token2token_id["<k>"]] = 1.0  # 枯
+    kdr_probabilities[0, 7, dataset.token2token_id["<k>"]] = 1.0  # れ
+    kdr_probabilities[0, 8, dataset.token2token_id["<k>"]] = 1.0  # る
+    kdr_probabilities[0, 9, dataset.token2token_id["<k>"]] = 1.0  # <dummy>
+    kdr_probabilities[0, 10, dataset.token2token_id["<k>"]] = 1.0  # [SEP]
+    kdr_probabilities[1, :, dataset.token2token_id["<k>"]] = 1.0
+    metric.kdr_probabilities, metric.kdr_predictions = kdr_probabilities.max(dim=2)
+    ins_probabilities = torch.zeros((2, max_seq_length, len(dataset.token2token_id)), dtype=torch.float)
+    ins_probabilities[0, :, dataset.token2token_id["<_>"]] = 1.0
+    ins_probabilities[1, 0, dataset.token2token_id["<_>"]] = 1.0  # [CLS]
+    ins_probabilities[1, 1, dataset.token2token_id["<_>"]] = 1.0  # 紹
+    ins_probabilities[1, 2, dataset.token2token_id["<_>"]] = 1.0  # 介
+    ins_probabilities[1, 3, dataset.token2token_id["する"]] = 1.0  # こ -> するこ
+    ins_probabilities[1, 4, dataset.token2token_id["<_>"]] = 1.0  # と
+    ins_probabilities[1, 5, dataset.token2token_id["が"]] = 1.0  # な -> がな
+    ins_probabilities[1, 6, dataset.token2token_id["<_>"]] = 1.0  # か
+    ins_probabilities[1, 7, dataset.token2token_id["<_>"]] = 1.0  # っ
+    ins_probabilities[1, 8, dataset.token2token_id["<_>"]] = 1.0  # た
+    ins_probabilities[1, 9, dataset.token2token_id["<_>"]] = 1.0  # <dummy>
+    ins_probabilities[1, 10, dataset.token2token_id["<_>"]] = 1.0  # [SEP]
+    metric.ins_probabilities, metric.ins_predictions = ins_probabilities.max(dim=2)
+
+    digits = 4
+    metrics = {k: round(v, digits) for k, v in metric.compute().items()}
+    # tp = 5, fp = 0, fn = 0
+    assert metrics["typo_correction_0.0_precision"] == round(5 / 5, digits)
+    assert metrics["typo_correction_0.0_recall"] == round(5 / 5, digits)
+    assert metrics["typo_correction_0.0_f1"] == round((2 * 5 / 5 * 5 / 5) / (5 / 5 + 5 / 5), digits)
+    assert metrics["typo_correction_0.0_f0.5"] == round((1.25 * 5 / 5 * 5 / 5) / (0.25 * 5 / 5 + 5 / 5), digits)
+    # tp = 4, fp = 0, fn = 1
+    assert metrics["typo_correction_0.8_precision"] == round(4 / 4, digits)
+    assert metrics["typo_correction_0.8_recall"] == round(4 / 5, digits)
+    assert metrics["typo_correction_0.8_f1"] == round((2 * 4 / 4 * 4 / 5) / (4 / 4 + 4 / 5), digits)
+    assert metrics["typo_correction_0.8_f0.5"] == round((1.25 * 4 / 4 * 4 / 5) / (0.25 * 4 / 4 + 4 / 5), digits)
+    # tp = 3, fp = 0, fn = 2
+    assert metrics["typo_correction_0.9_precision"] == round(3 / 3, digits)
+    assert metrics["typo_correction_0.9_recall"] == round(3 / 5, digits)
+    assert metrics["typo_correction_0.9_f1"] == round((2 * 3 / 3 * 3 / 5) / (3 / 3 + 3 / 5), digits)
+    assert metrics["typo_correction_0.9_f0.5"] == round((1.25 * 3 / 3 * 3 / 5) / (0.25 * 3 / 3 + 3 / 5), digits)
