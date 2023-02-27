76f154dea311a31971a905fc4f57ffbd6c67ad4c
nobu-g <ueda-n127@hotmail.co.jp>
2023-02-28 04:13:28 +0900

    refactor CLIProcessor

--- src/kwja/cli/cli.py
+++ src/kwja/cli/cli.py
@@ -3,7 +3,7 @@ import re
 from enum import Enum
 from pathlib import Path
 from tempfile import TemporaryDirectory
-from typing import Any, Dict, List, Optional
+from typing import Dict, List, Optional, Set, Tuple
 
 import hydra
 import pytorch_lightning as pl
@@ -32,6 +32,146 @@ class Device(str, Enum):
     gpu = "gpu"
 
 
+class BaseModuleProcessor:
+    def __init__(
+        self,
+        specified_device: str,
+        model_size: str,
+        batch_size: int,
+        destination: Path,
+    ) -> None:
+        self.device_name, self.device = prepare_device(specified_device)
+        self.model_size: str = model_size
+        self.batch_size: int = batch_size
+        self.destination: Path = destination
+        self.module: Optional[pl.LightningModule] = None
+        self.trainer: Optional[pl.Trainer] = None
+
+    def load(self):
+        self.module = self._load_module()
+        self.module.hparams.datamodule.batch_size = self.batch_size
+
+        writer = hydra.utils.instantiate(self.module.hparams.callbacks.prediction_writer, destination=self.destination)
+        self.trainer = pl.Trainer(
+            logger=False,
+            callbacks=[writer, hydra.utils.instantiate(self.module.hparams.callbacks.progress_bar)],
+            accelerator=self.device_name,
+            devices=1,
+        )
+
+    def _load_module(self) -> pl.LightningModule:
+        raise NotImplementedError()
+
+    def delete_module_and_trainer(self) -> None:
+        del self.module, self.trainer
+
+    def apply_module(self, input_file: Path) -> Path:
+        datamodule = self._load_datamodule(input_file)
+        assert self.trainer is not None
+        self.trainer.predict(model=self.module, dataloaders=datamodule.predict_dataloader(), return_predictions=False)
+        return self.destination
+
+    def _load_datamodule(self, input_file: Path) -> DataModule:
+        raise NotImplementedError()
+
+    def output_prediction(self) -> None:
+        raise NotImplementedError()
+
+
+class TypoModuleProcessor(BaseModuleProcessor):
+    def __init__(self, specified_device: str, model_size: str, batch_size: int, destination: Path) -> None:
+        super().__init__(specified_device, model_size, batch_size, destination)
+
+    def _load_module(self) -> pl.LightningModule:
+        typer.echo("Loading typo module", err=True)
+        checkpoint_path: Path = download_checkpoint(task="typo", model_size=self.model_size)
+        return TypoModule.load_from_checkpoint(checkpoint_path, map_location=self.device)
+
+    def _load_datamodule(self, input_file: Path) -> DataModule:
+        assert self.module is not None
+        self.module.hparams.datamodule.predict.texts = input_file.read_text().splitlines()
+        datamodule = DataModule(cfg=self.module.hparams.datamodule)
+        datamodule.setup(stage=TrainerFn.PREDICTING)
+        return datamodule
+
+    def output_prediction(self) -> None:
+        post_texts: List[str] = []
+        with self.destination.open() as f:
+            for line in f:
+                if line.strip() != "EOD":
+                    post_texts.append(line.strip())
+        print("\n".join(post_texts))
+
+
+class CharModuleProcessor(BaseModuleProcessor):
+    def __init__(self, specified_device: str, model_size: str, batch_size: int, destination: Path) -> None:
+        super().__init__(specified_device, model_size, batch_size, destination)
+
+    def _load_module(self) -> pl.LightningModule:
+        typer.echo("Loading char module", err=True)
+        checkpoint_path: Path = download_checkpoint(task="char", model_size=self.model_size)
+        return CharModule.load_from_checkpoint(checkpoint_path, map_location=self.device)
+
+    def _load_datamodule(self, input_file: Path) -> DataModule:
+        assert self.module is not None
+        self.module.hparams.datamodule.predict.texts = input_file.read_text().splitlines()
+        datamodule = DataModule(cfg=self.module.hparams.datamodule)
+        datamodule.setup(stage=TrainerFn.PREDICTING)
+        return datamodule
+
+    def output_prediction(self) -> None:
+        word_segmented_texts: List[str] = []
+        with self.destination.open() as f:
+            for juman_text in chunk_by_document(f):
+                document = Document.from_jumanpp(juman_text)
+                word_segmented_texts.append(" ".join(m.text for m in document.morphemes))
+        print("\n".join(word_segmented_texts))
+
+
+class WordModuleProcessor(BaseModuleProcessor):
+    def __init__(self, specified_device: str, model_size: str, batch_size: int, destination: Path) -> None:
+        super().__init__(specified_device, model_size, batch_size, destination)
+
+    def _load_module(self) -> pl.LightningModule:
+        typer.echo("Loading word module", err=True)
+        checkpoint_path: Path = download_checkpoint(task="word", model_size=self.model_size)
+        return WordModule.load_from_checkpoint(checkpoint_path, map_location=self.device)
+
+    def _load_datamodule(self, input_file: Path) -> DataModule:
+        assert self.module is not None
+        self.module.hparams.datamodule.predict.juman_file = input_file
+        datamodule = DataModule(cfg=self.module.hparams.datamodule)
+        datamodule.setup(stage=TrainerFn.PREDICTING)
+        return datamodule
+
+    def output_prediction(self) -> None:
+        print(self.destination.read_text(), end="")
+
+
+class WordDiscourseModuleProcessor(BaseModuleProcessor):
+    def __init__(self, specified_device: str, model_size: str, batch_size: int, destination: Path) -> None:
+        super().__init__(specified_device, model_size, batch_size, destination)
+
+    def _load_module(self) -> pl.LightningModule:
+        typer.echo("Loading word_discourse module", err=True)
+        checkpoint_path: Path = download_checkpoint(task="word_discourse", model_size=self.model_size)
+        module = WordModule.load_from_checkpoint(checkpoint_path, map_location=self.device)
+        module.hparams.callbacks.prediction_writer = {
+            "_target_": "kwja.callbacks.word_discourse_module_writer.WordDiscourseModuleWriter"
+        }
+        return module
+
+    def _load_datamodule(self, input_file: Path) -> DataModule:
+        assert self.module is not None
+        self.module.hparams.datamodule.predict.knp_file = input_file
+        datamodule = DataModule(cfg=self.module.hparams.datamodule)
+        datamodule.setup(stage=TrainerFn.PREDICTING)
+        return datamodule
+
+    def output_prediction(self) -> None:
+        print(self.destination.read_text(), end="")
+
+
 class CLIProcessor:
     def __init__(
         self,
@@ -41,136 +181,66 @@ class CLIProcessor:
         char_batch_size: int,
         word_batch_size: int,
     ) -> None:
-        self.device_name, self.device = prepare_device(specified_device)
-        self.model_size = model_size
-
-        self.tmp_dir: TemporaryDirectory = TemporaryDirectory()
-        self.task2task_settings: Dict[str, Any] = {
-            "typo": {
-                "module_class": TypoModule,
-                "batch_size": typo_batch_size,
-                "destination": self.tmp_dir.name / Path("typo_prediction.txt"),
-            },
-            "char": {
-                "module_class": CharModule,
-                "batch_size": char_batch_size,
-                "destination": self.tmp_dir.name / Path("char_prediction.juman"),
-            },
-            "word": {
-                "module_class": WordModule,
-                "batch_size": word_batch_size,
-                "destination": self.tmp_dir.name / Path("word_prediction.knp"),
-            },
-            "word_discourse": {
-                "module_class": WordModule,
-                "batch_size": word_batch_size,
-                "destination": self.tmp_dir.name / Path("word_discourse_prediction.knp"),
-            },
+        self.tmp_dir = TemporaryDirectory()
+        self.raw_destination = self.tmp_dir.name / Path("raw_text.txt")
+        self.typo_destination = self.tmp_dir.name / Path("typo_prediction.txt")
+        self.char_destination = self.tmp_dir.name / Path("char_prediction.juman")
+        self.word_destination = self.tmp_dir.name / Path("word_prediction.knp")
+        self.word_discourse_destination = self.tmp_dir.name / Path("word_discourse_prediction.knp")
+        self.processors: Dict[str, BaseModuleProcessor] = {
+            "typo": TypoModuleProcessor(specified_device, model_size, typo_batch_size, self.typo_destination),
+            "char": CharModuleProcessor(specified_device, model_size, char_batch_size, self.char_destination),
+            "word": WordModuleProcessor(specified_device, model_size, word_batch_size, self.word_destination),
+            "word_discourse": WordDiscourseModuleProcessor(
+                specified_device, model_size, word_batch_size, self.word_discourse_destination
+            ),
         }
 
-    @staticmethod
-    def _split_input_texts(input_texts: List[str]) -> List[str]:
-        split_texts: List[str] = []
-        split_text: str = ""
-        for input_text in input_texts:
-            stripped_input_text: str = input_text.strip()
-            if stripped_input_text.endswith("EOD"):
-                stripped_input_text = stripped_input_text[:-3]
-            input_text_with_eod: str = stripped_input_text + "\nEOD"
-            for text in input_text_with_eod.split("\n"):
-                if text == "EOD":
-                    # hydra.utils.instantiateを実行する際に文字列${...}を補間しようとするのを防ぐ
-                    normalized = OMEGACONF_VARIABLE_INTERPOLATION.sub(r"$␣\g<variable>", split_text)
-                    # "#"で始まる行がコメント行と誤認識されることを防ぐ
-                    normalized = normalized.replace("#", "♯")
-                    split_texts.append(normalized.rstrip())
-                    split_text = ""
-                else:
-                    split_text += f"{text}\n"
-        return split_texts
+    def load_modules(self, tasks: List[str]) -> None:
+        for task in tasks:
+            self.processors[task].load()
 
-    def load_module(self, task: str) -> None:
-        typer.echo(f"Loading {task} module", err=True)
-
-        checkpoint_path: Path = download_checkpoint(task=task, model_size=self.model_size)
-        module = self.task2task_settings[task]["module_class"].load_from_checkpoint(
-            str(checkpoint_path), map_location=self.device
-        )
-        module.hparams.datamodule.batch_size = self.task2task_settings[task]["batch_size"]
-        self.task2task_settings[task]["module"] = module
-
-        if task == "word_discourse":
-            module.hparams.callbacks.prediction_writer = {
-                "_target_": "kwja.callbacks.word_discourse_module_writer.WordDiscourseModuleWriter"
-            }
-        writer = hydra.utils.instantiate(
-            module.hparams.callbacks.prediction_writer, destination=self.task2task_settings[task]["destination"]
-        )
-        trainer = pl.Trainer(
-            logger=False,
-            callbacks=[writer, hydra.utils.instantiate(module.hparams.callbacks.progress_bar)],
-            accelerator=self.device_name,
-            devices=1,
-        )
-        self.task2task_settings[task]["trainer"] = trainer
-
-    def delete_module_and_trainer(self, task: str) -> None:
-        del self.task2task_settings[task]["module"], self.task2task_settings[task]["trainer"]
-
-    def apply_module(self, task: str, input_texts: Optional[List[str]] = None):
-        module = self.task2task_settings[task]["module"]
-        if input_texts is not None:
-            module.hparams.datamodule.predict.texts = self._split_input_texts(input_texts)
-        else:
-            if task == "char":
-                module.hparams.datamodule.predict.texts = self._split_input_texts(
-                    [self.task2task_settings["typo"]["destination"].read_text()]
-                )
-            elif task == "word":
-                module.hparams.datamodule.predict.juman_file = self.task2task_settings["char"]["destination"]
-            elif task == "word_discourse":
-                module.hparams.datamodule.predict.knp_file = self.task2task_settings["word"]["destination"]
-
-        datamodule = DataModule(cfg=module.hparams.datamodule)
-        datamodule.setup(stage=TrainerFn.PREDICTING)
-
-        trainer = self.task2task_settings[task]["trainer"]
-        trainer.predict(model=module, dataloaders=datamodule.predict_dataloader(), return_predictions=False)
-
-    def output_prediction(self, task: str) -> None:
-        if task == "typo":
-            post_texts: List[str] = []
-            with self.task2task_settings[task]["destination"].open(mode="r") as f:
-                for line in f:
-                    if line.strip() != "EOD":
-                        post_texts.append(line.strip())
-            print("\n".join(post_texts))
-        elif task == "char":
-            word_segmented_texts: List[str] = []
-            with self.task2task_settings[task]["destination"].open(mode="r") as f:
-                for juman_text in chunk_by_document(f):
-                    document = Document.from_jumanpp(juman_text)
-                    word_segmented_texts.append(" ".join(m.text for m in document.morphemes))
-            print("\n".join(word_segmented_texts))
-        else:
-            print(self.task2task_settings[task]["destination"].read_text(), end="")
-
-    def refresh(self):
-        for task_settings in self.task2task_settings.values():
-            task_settings["destination"].unlink(missing_ok=True)
+    def refresh(self) -> None:
+        for processor in self.processors.values():
+            processor.destination.unlink(missing_ok=True)
 
     def run(self, input_text: str, specified_tasks: List[str], interactive: bool = False) -> None:
+        input_texts = _split_input_texts([input_text])
+        self.raw_destination.write_text("\n".join(input_texts))  # TODO: consider using pickle
+        input_file = self.raw_destination
         for specified_task in specified_tasks:
+            processor = self.processors[specified_task]
             if interactive is False:
-                self.load_module(specified_task)
-            if (specified_task == "typo") or (specified_task == "char" and "typo" not in specified_tasks):
-                input_texts = [input_text]
+                processor.load()
+            if specified_task == "char" and "typo" in specified_tasks:
+                # char module after typo module
+                input_texts = _split_input_texts([input_file.read_text()])
+                input_file.write_text("\n".join(input_texts))  # TODO: consider using pickle
+            input_file = processor.apply_module(input_file)
+            if interactive is False:
+                processor.delete_module_and_trainer()
+        self.processors[specified_tasks[-1]].output_prediction()
+
+
+def _split_input_texts(input_texts: List[str]) -> List[str]:
+    split_texts: List[str] = []
+    split_text: str = ""
+    for input_text in input_texts:
+        stripped_input_text: str = input_text.strip()
+        if stripped_input_text.endswith("EOD"):
+            stripped_input_text = stripped_input_text[:-3]
+        input_text_with_eod: str = stripped_input_text + "\nEOD"
+        for text in input_text_with_eod.split("\n"):
+            if text == "EOD":
+                # hydra.utils.instantiateを実行する際に文字列${...}を補間しようとするのを防ぐ
+                normalized = OMEGACONF_VARIABLE_INTERPOLATION.sub(r"$␣\g<variable>", split_text)
+                # "#"で始まる行がコメント行と誤認識されることを防ぐ
+                normalized = normalized.replace("#", "♯")
+                split_texts.append(normalized.rstrip())
+                split_text = ""
             else:
-                input_texts = None
-            self.apply_module(specified_task, input_texts=input_texts)
-            if interactive is False:
-                self.delete_module_and_trainer(specified_task)
-        self.output_prediction(specified_tasks[-1])
+                split_text += f"{text}\n"
+    return split_texts
 
 
 def version_callback(value: bool) -> None:
@@ -180,7 +250,7 @@ def version_callback(value: bool) -> None:
 
 
 def model_size_callback(value: str) -> str:
-    if value not in ["tiny", "base", "large"]:
+    if value not in ("tiny", "base", "large"):
         raise typer.BadParameter("model size must be one of 'tiny', 'base', or 'large'")
     return value
 
@@ -190,18 +260,18 @@ def tasks_callback(value: str) -> str:
     if len(tasks) == 0:
         raise typer.BadParameter("task must be specified")
     for task in tasks:
-        if task not in ["typo", "char", "word", "word_discourse"]:
+        if task not in ("typo", "char", "word", "word_discourse"):
             raise typer.BadParameter("invalid task name is contained")
-    valid_task_combinations: List[List[str]] = [
-        ["typo"],
-        ["char"],
-        ["char", "typo"],
-        ["char", "word"],
-        ["char", "typo", "word"],
-        ["char", "word", "word_discourse"],
-        ["char", "typo", "word", "word_discourse"],
-    ]
-    sorted_task: List[str] = sorted(tasks)
+    valid_task_combinations: Set[Tuple[str, ...]] = {
+        ("typo",),
+        ("char",),
+        ("char", "typo"),
+        ("char", "word"),
+        ("char", "typo", "word"),
+        ("char", "word", "word_discourse"),
+        ("char", "typo", "word", "word_discourse"),
+    }
+    sorted_task: Tuple[str, ...] = tuple(sorted(tasks))
     if sorted_task not in valid_task_combinations:
         raise typer.BadParameter(
             "task combination is invalid. "
@@ -241,8 +311,7 @@ def main(
     elif text is not None:
         input_text = text
     elif filename is not None:
-        with Path(filename).open() as f:
-            input_text = f.read()
+        input_text = Path(filename).read_text()
 
     processor = CLIProcessor(
         specified_device=device.value,
@@ -256,8 +325,7 @@ def main(
     ]
 
     if input_text is None:
-        for specified_task in specified_tasks:
-            processor.load_module(specified_task)
+        processor.load_modules(specified_tasks)
 
         typer.echo('Please end your input with a new line and type "EOD"', err=True)
         input_text = ""
