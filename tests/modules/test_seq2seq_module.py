a78764b63a03a72bb571e1d839e655a536f70e0c
Taka008 <kodama@nlp.ist.i.kyoto-u.ac.jp>
2023-03-09 20:03:12 +0900

    add test for Seq2SeqModule

---
+++ tests/modules/test_seq2seq_module.py
@@ -0,0 +1,48 @@
+from pathlib import Path
+
+import hydra
+from hydra import compose, initialize
+from hydra.core.hydra_config import HydraConfig
+from omegaconf import OmegaConf
+from torch.utils.data import DataLoader
+
+from kwja.datamodule.datamodule import dataclass_data_collator
+from kwja.datamodule.datasets import Seq2SeqDataset
+from kwja.modules import Seq2SeqModule
+
+with initialize(version_base=None, config_path="../../configs"):
+    cfg = compose(
+        config_name="seq2seq_module.debug",
+        return_hydra_config=True,
+        overrides=["max_src_length=32", "max_tgt_length=512"],
+    )
+    HydraConfig.instance().set_config(cfg)
+    OmegaConf.set_readonly(cfg.hydra, False)
+    OmegaConf.resolve(cfg)
+
+
+def test_init() -> None:
+    _ = Seq2SeqModule(cfg)
+
+
+def test_steps(fixture_data_dir: Path) -> None:
+    trainer = hydra.utils.instantiate(
+        cfg.trainer,
+        logger=None,
+        enable_checkpointing=False,
+        devices=1,
+        accelerator="cpu",
+    )
+
+    path = fixture_data_dir / "datasets" / "word_files"
+    seq2seq_tokenizer = hydra.utils.instantiate(cfg.datamodule.train.kyoto.tokenizer)
+    dataset = Seq2SeqDataset(str(path), seq2seq_tokenizer, cfg.max_src_length, cfg.max_tgt_length)
+    data_loader = DataLoader(dataset, batch_size=len(dataset), collate_fn=dataclass_data_collator)
+
+    cfg.datamodule.valid = {"dummy": ""}
+    cfg.datamodule.test = {"dummy": ""}
+    module = Seq2SeqModule(cfg)
+
+    trainer.fit(model=module, train_dataloaders=data_loader, val_dataloaders=data_loader)
+    trainer.test(model=module, dataloaders=data_loader)
+    trainer.predict(model=module, dataloaders=data_loader)
