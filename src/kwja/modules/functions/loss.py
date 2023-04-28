fix loss functions to average loss over examples

src/kwja/modules/functions/loss.py
@@ -3,16 +3,19 @@ import torch.nn.functional as F
 
 from kwja.utils.constants import IGNORE_INDEX, MASKED
 
+eps = 1e-6
+
 
 def compute_token_mean_loss(
-    input_: torch.Tensor,  # (b, seq, num_features)
-    target: torch.Tensor,  # (b, seq)
-) -> torch.Tensor:
-    return F.cross_entropy(
-        input=input_.view(-1, input_.size(-1)),
-        target=target.view(-1),
-        ignore_index=IGNORE_INDEX,
-    )
+    input_: torch.Tensor,  # (b, *seq, num_classes)
+    target: torch.Tensor,  # (b, *seq)
+) -> torch.Tensor:  # ()
+    batch_size, *seq, num_classes = input_.shape
+    input_ = input_.view(batch_size, -1, num_classes)  # (b, seq, num_classes)
+    target = target.view(batch_size, -1)  # (b, seq)
+    losses = F.cross_entropy(input_.transpose(1, 2), target, ignore_index=IGNORE_INDEX, reduction="none")  # (b, seq)
+    mask: torch.Tensor = target.ne(IGNORE_INDEX)  # (b, seq)
+    return ((losses * mask).sum(dim=1) / (mask.sum(dim=1) + eps)).mean()
 
 
 def compute_multi_label_token_mean_loss(
@@ -20,12 +23,11 @@ def compute_multi_label_token_mean_loss(
     target: torch.Tensor,  # (b, seq, num_features)
     mask: torch.Tensor,  # (b, seq, num_features)
 ) -> torch.Tensor:  # ()
-    num_units = mask[:, :, 0].sum(dim=1)  # (b, )
     # binary_cross_entropy は IGNORE_INDEX を渡せない
-    losses = F.binary_cross_entropy(input=input_, target=target, reduction="none")
+    losses = F.binary_cross_entropy(input_, target, reduction="none")  # (b, seq, num_features)
     # batch と sequence の軸は平均を、 label の軸は和をとる
-    losses = losses.sum(dim=1).sum(dim=1)  # (b, seq, num_features) -> (b, num_features) -> (b, )
-    return (losses / num_units).mean()
+    losses = (losses * mask).sum(dim=2)  # (b, seq)
+    return (losses.sum(dim=1) / (mask[:, :, 0].sum(dim=1) + eps)).mean()
 
 
 def compute_cohesion_analysis_loss(
@@ -33,8 +35,7 @@ def compute_cohesion_analysis_loss(
     target: torch.Tensor,  # (b, rel, seq, seq)
 ) -> torch.Tensor:  # ()
     log_softmax = torch.log_softmax(input_, dim=3)  # (b, rel, seq, seq)
-    denominator = target.sum() + 1e-6
-    return (-log_softmax * target).sum().div(denominator)
+    return ((-log_softmax * target).sum((1, 2, 3)) / (target.sum((1, 2, 3)) + eps)).mean()
 
 
 def mask_logits(logits: torch.Tensor, mask: torch.Tensor) -> torch.Tensor: