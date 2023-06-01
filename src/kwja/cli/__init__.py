bf1dc8723c2daa88ca66abddde7aa8c45d80b0ea
nobu-g <ueda-n127@hotmail.co.jp>
2023-06-01 20:27:04 +0900

    fix test for Windows

--- src/kwja/cli/__init__.py
+++ src/kwja/cli/__init__.py
@@ -1,9 +1,5 @@
 import os
-import platform
 import sys
 
 if "pytest" not in sys.modules:
     os.environ["KWJA_CLI_MODE"] = "1"
-
-if platform.system() == "Windows":
-    os.environ["PYTHONUTF8"] = "1"
