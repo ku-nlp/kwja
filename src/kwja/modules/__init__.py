8617c5fa6e068212d01afa9598d385dfcad327c0
Kazumasa Omura <omura@nlp.ist.i.kyoto-u.ac.jp>
2023-08-23 18:53:04 +0900

    fix modules

--- src/kwja/modules/__init__.py
+++ src/kwja/modules/__init__.py
@@ -1,13 +1,11 @@
 from .char import CharModule
-from .senter import SenterModule
 from .seq2seq import Seq2SeqModule
 from .typo import TypoModule
 from .word import WordModule
 
 __all__ = [
-    "SenterModule",
     "TypoModule",
-    "Seq2SeqModule",
     "CharModule",
+    "Seq2SeqModule",
     "WordModule",
 ]
