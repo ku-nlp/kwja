d92dc90beffcce3138062724167298f6aa77b730
Kazumasa Omura <omura@nlp.ist.i.kyoto-u.ac.jp>
2023-08-23 18:55:24 +0900

    fix scripts

--- scripts/deploy_dev_checkpoints.sh
+++ scripts/deploy_dev_checkpoints.sh
@@ -22,10 +22,10 @@ if [[ $# -ne 2 ]]; then
   usage
 fi
 
-for task in typo senter char seq2seq word; do
+for task in typo char seq2seq word; do
   train_extra_args=("ignore_hparams_on_save=true" "trainer=cpu.debug" "do_predict_after_train=false")
   module="${task}"
-  checkpoint_dir="result/${module}_module.debug-ignore_hparams_on_save_true-trainer_cpu.debug"
+  checkpoint_dir="result/${module}_module.debug-ignore_hparams_on_save=true-trainer=cpu.debug"
 
   poetry run python scripts/train.py -cn ${module}_module.debug "${train_extra_args[@]}"
 
@@ -37,12 +37,10 @@ for task in typo senter char seq2seq word; do
 
   if [[ "${module}" == "typo" ]]; then
     pretrained_model_name="deberta-v2-tiny-wwm"
-  elif [[ "${module}" == "senter" ]]; then
-    pretrained_model_name="deberta-v2-base-wwm"
   elif [[ "${module}" == "char" ]]; then
     pretrained_model_name="deberta-v2-tiny-wwm"
   elif [[ "${module}" == "seq2seq" ]]; then
-    pretrained_model_name="mt5-small"
+    pretrained_model_name="t5-small"
   elif [[ "${module}" == "word" ]]; then
     pretrained_model_name="deberta-v2-tiny"
   else
