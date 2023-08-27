d57fa6e580a825da7688006813acc8003d811e90
nobu-g <ueda-n127@hotmail.co.jp>
2023-08-27 22:45:10 +0900

    make --device option case insensitive

--- src/kwja/cli/cli.py
+++ src/kwja/cli/cli.py
@@ -276,7 +276,7 @@ def main(
     text: Annotated[Optional[str], typer.Option(help="Text to be analyzed.")] = None,
     filename: List[Path] = typer.Option([], dir_okay=False, help="Files to be analyzed."),
     model_size: Annotated[Optional[ModelSize], typer.Option(help="Model size to be used.")] = None,
-    device: Annotated[Optional[Device], typer.Option(help="Device to be used.")] = None,
+    device: Annotated[Optional[Device], typer.Option(case_sensitive=False, help="Device to be used.")] = None,
     typo_batch_size: Annotated[Optional[int], typer.Option(help="Batch size for typo module.")] = None,
     char_batch_size: Annotated[Optional[int], typer.Option(help="Batch size for char module.")] = None,
     seq2seq_batch_size: Annotated[Optional[int], typer.Option(help="Batch size for seq2seq module.")] = None,
